877aaf443f94b1a65c1b20a22ba9190c90307fce
Carles Sala <carles@pythiac.com>
2020-12-28 22:07:19 +0100

    Ensure binary classification targets are bool (#33)
    * Ensure binary classification targets are bool
    
    * Make good data worse, and bad data much worse

--- tests/integration/single_table/efficacy/test_binary.py
+++ tests/integration/single_table/efficacy/test_binary.py
@@ -14,31 +14,43 @@ METRICS = [
 ]
 
 
-def real_data():
-    return pd.DataFrame({
+def real_data(as_str=False):
+    data = pd.DataFrame({
         'a': np.random.normal(size=600),
         'b': np.random.randint(0, 10, size=600),
         'c': ['a', 'b', 'b', 'c', 'c', 'c'] * 100,
         'd': [True, True, True, True, True, False] * 100,
     })
+    if as_str:
+        data['d'] = data['d'].astype(str)
+
+    return data
 
 
-def good_data():
-    return pd.DataFrame({
+def good_data(as_str=False):
+    data = pd.DataFrame({
         'a': np.random.normal(loc=0.01, size=600),
         'b': np.random.randint(0, 10, size=600),
         'c': ['a', 'b', 'b', 'b', 'c', 'c'] * 100,
         'd': [True, True, True, True, False, False] * 100,
     })
+    if as_str:
+        data['d'] = data['d'].astype(str)
+
+    return data
 
 
-def bad_data():
-    return pd.DataFrame({
+def bad_data(as_str=False):
+    data = pd.DataFrame({
         'a': np.random.normal(loc=5, scale=3, size=600),
         'b': np.random.randint(5, 15, size=600),
         'c': ['a', 'a', 'a', 'a', 'b', 'b'] * 100,
         'd': [True, False, False, False, False, False] * 100,
     })
+    if as_str:
+        data['d'] = data['d'].astype(str)
+
+    return data
 
 
 @pytest.mark.parametrize('metric', METRICS)
@@ -46,5 +58,14 @@ def test_rank(metric):
     bad = metric.compute(real_data(), bad_data(), target='d')
     good = metric.compute(real_data(), good_data(), target='d')
     real = metric.compute(real_data(), real_data(), target='d')
+
+    assert metric.min_value <= bad < good <= real <= metric.max_value
+
+
+@pytest.mark.parametrize('metric', METRICS)
+def test_rank_object(metric):
+    bad = metric.compute(real_data(True), bad_data(True), target='d')
+    good = metric.compute(real_data(True), good_data(True), target='d')
+    real = metric.compute(real_data(True), real_data(True), target='d')
 
     assert metric.min_value <= bad < good <= real <= metric.max_value
