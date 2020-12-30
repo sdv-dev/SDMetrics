2b3f4c02f43e6c7f9695903c17ee1ddee985eb25
Carles Sala <carles@pythiac.com>
2020-12-30 21:09:46 +0100

    Bump version: 0.1.1.dev1 → 0.1.1

--- setup.py
+++ setup.py
@@ -101,6 +101,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/SDMetrics',
-    version='0.1.1.dev1',
+    version='0.1.1',
     zip_safe=False,
 )
