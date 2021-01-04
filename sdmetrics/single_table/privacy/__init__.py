41177809381bcfbd3b027fb475bb3266ddc8d5cd
Zhuofan Xie <zhuofanx@mit.edu>
2021-01-03 19:59:54 -0500

    implemented CAP related categorical metrics

--- sdmetrics/single_table/privacy/__init__.py
+++ sdmetrics/single_table/privacy/__init__.py
@@ -1,6 +1,16 @@
-from sdmetrics.single_table.privacy.base import CategoricalType, CatPrivacyMetric
+from sdmetrics.single_table.privacy.base import CategoricalType, CatPrivacyMetric,\
+    PrivacyAttackerModel
+from sdmetrics.single_table.privacy.CAP import CAPAttacker, CAP, GCAPAttacker, GCAP,\
+    ZeroCAPAttacker, ZeroCAP
 
 __all__ = [
     'CatPrivacyMetric',
     'CategoricalType',
+    'PrivacyAttackerModel',
+    'CAPAttacker',
+    'CAP',
+    'GCAPAttacker',
+    'GCAP',
+    'ZeroCAPAttacker',
+    'ZeroCAP',
 ]
