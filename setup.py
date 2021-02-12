900f2d3d74e0ba7e17a99fc7ae907f37fbbae360
fealho <fealho@gmail.com>
2021-02-12 12:30:18 -0800

    Fixes the sktime issue for conda (#50)
    * Fixes the sktime issue for conda
    
    * Makes changes backward compatible.
    
    * Remove unnecessary dependencies
    
    * Fix version

--- setup.py
+++ setup.py
@@ -12,16 +12,13 @@ with open('HISTORY.md', encoding='utf-8') as history_file:
     history = history_file.read()
 
 install_requires = [
-    'scikit-learn>=0.20,<1',
-    'sktime>=0.4,<0.5',
-    'tsfresh>=0.15,<1',
-    'scipy>=1.1.0,<1.6.0',
     'numpy>=1.15.4,<2',
     'pandas>=1,<1.1.5',
-    'seaborn>=0.9,<0.11',
-    'matplotlib>=2.2.2,<3.2.2',
     'pomegranate>=0.13.0,<0.13.5',
     'rdt>=0.2.10,<0.4',
+    'scikit-learn>=0.20,<1',
+    'scipy>=1.1.0,<1.6.0',
+    'sktime>=0.4,<0.6',
     'torch>=1,<2',
 ]
 
