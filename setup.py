9e9e61926bea6460b05875404cc8d6d830d9a303
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2021-03-29 23:56:11 +0200

    Bump version: 0.2.1.dev1 → 0.3.0.dev0

--- setup.py
+++ setup.py
@@ -99,6 +99,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/SDMetrics',
-    version='0.2.1.dev1',
+    version='0.3.0.dev0',
     zip_safe=False,
 )
