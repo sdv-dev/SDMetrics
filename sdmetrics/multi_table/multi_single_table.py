33ff2f2300b2dd5b5cba92eae684a03af43b9e5a
Felipe Hofmann <fealho@gmail.com>
2021-11-04 16:12:34 -0700

    Fix D401

--- sdmetrics/multi_table/multi_single_table.py
+++ sdmetrics/multi_table/multi_single_table.py
@@ -103,7 +103,7 @@ class MultiSingleTableMetric(MultiTableMetric, metaclass=NestedAttrsMeta('single
 
     @classmethod
     def normalize(cls, raw_score):
-        """Returns the `raw_score` as is, since it is already normalized.
+        """Return the `raw_score` as is, since it is already normalized.
 
         Args:
             raw_score (float):
