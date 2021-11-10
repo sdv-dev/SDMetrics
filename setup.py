2d26d3d7525ac866c7daeaa6c2e88ed338396550
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2021-11-10 15:05:19 +0100

    Fix 3.6 dependancy

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('HISTORY.md', encoding='utf-8') as history_file:
     history = history_file.read()
 
 install_requires = [
-    "numpy>=1.18.0,<1.20.0;python_version<'3.7'",
+    "numpy>=1.19.0,<1.20.0;python_version<'3.7'",
     "numpy>=1.20.0,<2;python_version>='3.7'",
     'pandas>=1.1.3,<1.1.5',
     'scikit-learn>=0.24,<2',
