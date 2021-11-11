9e49ce641d5c154e81bed5514a1d5685b8d68d0b
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2021-11-11 13:09:48 +0100

    Add python 3.9 support

--- setup.py
+++ setup.py
@@ -98,6 +98,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description='Metrics for Synthetic Data Generation Projects',
     extras_require={
@@ -114,7 +115,7 @@ setup(
     keywords='sdmetrics sdmetrics SDMetrics',
     name='sdmetrics',
     packages=find_packages(include=['sdmetrics', 'sdmetrics.*']),
-    python_requires='>=3.6,<3.9',
+    python_requires='>=3.6,<3.10',
     setup_requires=setup_requires,
     test_suite='tests',
     tests_require=tests_require,
