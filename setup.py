a04471d51e687c9e0939cd87315e05b61cc79ed3
Felipe Hofmann <fealho@gmail.com>
2022-05-10 14:09:14 -0700

    Bump version: 0.4.2.dev1 → 0.4.2

--- setup.py
+++ setup.py
@@ -119,6 +119,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/SDMetrics',
-    version='0.4.2.dev1',
+    version='0.4.2',
     zip_safe=False,
 )
