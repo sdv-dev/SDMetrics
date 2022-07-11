48ac845829c174489395ed7361ce042b47fc94e0
Katharine Xiao <2405771+katxiao@users.noreply.github.com>
2022-07-11 12:52:42 -0400

    Add category coverage metric

--- sdmetrics/single_column/statistical/__init__.py
+++ sdmetrics/single_column/statistical/__init__.py
@@ -1,11 +1,13 @@
 """Univariate goodness-of-fit tests."""
 
+from sdmetrics.single_column.statistical.category_coverage import CategoryCoverage
 from sdmetrics.single_column.statistical.cstest import CSTest
 from sdmetrics.single_column.statistical.kscomplement import KSComplement
 from sdmetrics.single_column.statistical.missing_value_similarity import MissingValueSimilarity
 from sdmetrics.single_column.statistical.statistic_similarity import StatisticSimilarity
 
 __all__ = [
+    'CategoryCoverage',
     'CSTest',
     'KSComplement',
     'MissingValueSimilarity',
