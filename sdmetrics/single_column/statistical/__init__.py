d0eb00f8e00ce8580ab033b04ef62bb4dbbd6ffb
Katharine Xiao <2405771+katxiao@users.noreply.github.com>
2022-07-12 21:42:15 -0400

    Add missing value similarity metric (#154)
    * Add missing value similarity metric
    
    * cr comments

--- sdmetrics/single_column/statistical/__init__.py
+++ sdmetrics/single_column/statistical/__init__.py
@@ -4,6 +4,7 @@ from sdmetrics.single_column.statistical.boundary_adherence import BoundaryAdher
 from sdmetrics.single_column.statistical.category_coverage import CategoryCoverage
 from sdmetrics.single_column.statistical.cstest import CSTest
 from sdmetrics.single_column.statistical.kscomplement import KSComplement
+from sdmetrics.single_column.statistical.missing_value_similarity import MissingValueSimilarity
 from sdmetrics.single_column.statistical.statistic_similarity import StatisticSimilarity
 
 __all__ = [
@@ -11,5 +12,6 @@ __all__ = [
     'CategoryCoverage',
     'CSTest',
     'KSComplement',
+    'MissingValueSimilarity',
     'StatisticSimilarity'
 ]
