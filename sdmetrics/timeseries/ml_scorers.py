6066f4e3ccf1e8aaa8671c4ac4afb2a4badd445d
Katharine Xiao <2405771+katxiao@users.noreply.github.com>
2022-07-22 16:38:19 -0400

    Replace LabelEncodingTransformer

--- sdmetrics/timeseries/ml_scorers.py
+++ sdmetrics/timeseries/ml_scorers.py
@@ -2,10 +2,10 @@
 
 import numpy as np
 import pandas as pd
-import rdt
 import torch
 from pyts.classification import TimeSeriesForest
 from pyts.multivariate.classification import MultivariateClassifier
+from sklearn.preprocessing import LabelEncoder
 
 
 def _stack(row):
@@ -48,15 +48,14 @@ def lstm_classifier(X_train, X_test, y_train, y_test):
     X_train = _x_to_packed_sequence(X_train).to(device)
     X_test = _x_to_packed_sequence(X_test).to(device)
 
-    transformer = rdt.transformers.categorical.LabelEncodingTransformer()
+    transformer = LabelEncoder()
     column = 'target'
-    output_column = f'{column}.value'
     y_train = pd.DataFrame(y_train, columns=[column])
     y_test = pd.DataFrame(y_test, columns=[column])
 
-    y_train = transformer.fit_transform(y_train, column)[output_column].to_numpy()
+    y_train = transformer.fit_transform(y_train[column])
     y_train = torch.LongTensor(y_train).to(device)
-    y_test = torch.LongTensor(transformer.transform(y_test)[output_column].to_numpy()).to(device)
+    y_test = torch.LongTensor(transformer.transform(y_test)).to(device)
 
     optimizer = torch.optim.Adam(list(lstm.parameters()) + list(linear.parameters()), lr=1e-2)
 
