3676a1e50caa34428e6bb3b37418581f392acf55
Katharine Xiao <2405771+katxiao@users.noreply.github.com>
2022-09-13 11:39:54 -0400

    Update error handling for reports (#221)
    * Add error handling logic
    
    * Add unit tests

--- tests/unit/reports/multi_table/test_multi_table_quality_report.py
+++ tests/unit/reports/multi_table/test_multi_table_quality_report.py
@@ -1,5 +1,5 @@
 import pickle
-from unittest.mock import Mock, mock_open, patch
+from unittest.mock import Mock, call, mock_open, patch
 
 import numpy as np
 import pandas as pd
@@ -133,6 +133,123 @@ class TestQualityReport:
             'Column Shapes': 0.15000000000000002,
             'Column Pair Trends': 0.15000000000000002,
             'Parent Child Relationships': 1.0,
+        }
+
+    @patch('sdmetrics.reports.multi_table.quality_report.discretize_and_apply_metric')
+    def test_generate_with_errored_metric(self, mock_discretize_and_apply_metric):
+        """Test the ``generate`` method with an errored metric.
+
+        Expect that the multi-table metrics are called. Expect that the aggregate scores
+        are computed without the errored metric.
+
+        Setup:
+        - Mock the expected multi-table metric compute breakdown calls.
+
+        Input:
+        - Real data.
+        - Synthetic data.
+        - Metadata.
+
+        Side Effects:
+        - Expect that each multi table metric's ``compute_breakdown`` methods are called once.
+        - Expect that the ``_overall_quality_score`` and ``_property_breakdown`` attributes
+          are populated.
+        """
+        # Setup
+        mock_discretize_and_apply_metric.return_value = {}
+        real_data = {
+            'table1': pd.DataFrame({'col1': [1, 2, 3], 'col2': ['a', 'b', 'c']}),
+            'table2': pd.DataFrame({'col1': [1, 1, 1]}),
+        }
+        synthetic_data = {
+            'table1': pd.DataFrame({'col1': [1, 3, 3], 'col2': ['b', 'b', 'c']}),
+            'table2': pd.DataFrame({'col1': [3, 1, 3]}),
+        }
+        metadata = {
+            'tables': {
+                'table1': {'col1': {'type': 'numerical'}, 'col2': {'type': 'categorical'}},
+                'table2': {'col1': {'type': 'numerical'}},
+            },
+        }
+
+        ks_complement_mock = Mock()
+        ks_complement_mock.__name__ = 'KSComplement'
+        ks_complement_mock.compute_breakdown.return_value = {
+            'table1': {
+                'col1': {'score': 0.1},
+                'col2': {'error': 'test error'},
+            }
+        }
+
+        tv_complement_mock = Mock()
+        tv_complement_mock.__name__ = 'TVComplement'
+        tv_complement_mock.compute_breakdown.return_value = {
+            'table1': {
+                'col1': {'score': 0.1},
+                'col2': {'score': 0.2},
+            }
+        }
+
+        corr_sim_mock = Mock()
+        corr_sim_mock.__name__ = 'CorrelationSimilarity'
+        corr_sim_mock.compute_breakdown.return_value = {
+            'table1': {
+                'col1': {'score': 0.1},
+                'col2': {'score': 0.2},
+            }
+        }
+
+        cont_sim_mock = Mock()
+        cont_sim_mock.__name__ = 'ContingencySimilarity'
+        cont_sim_mock.compute_breakdown.return_value = {
+            'table1': {
+                'col1': {'score': 0.1},
+                'col2': {'score': 0.2},
+            }
+        }
+
+        cardinality_mock = Mock()
+        cardinality_mock.__name__ = 'CardinalityShapeSimilarity'
+        cardinality_mock.compute_breakdown.return_value = {
+            ('table1', 'table2'): {'score': 1.0},
+        }
+        metrics_mock = {
+            'Column Shapes': [ks_complement_mock, tv_complement_mock],
+            'Column Pair Trends': [corr_sim_mock, cont_sim_mock],
+            'Parent Child Relationships': [cardinality_mock],
+        }
+
+        # Run
+        with patch.object(
+            QualityReport,
+            'METRICS',
+            metrics_mock,
+        ):
+            report = QualityReport()
+            report.generate(real_data, synthetic_data, metadata)
+
+        # Assert
+        ks_complement_mock.compute_breakdown.assert_called_once_with(
+            real_data, synthetic_data, metadata)
+        tv_complement_mock.compute_breakdown.assert_called_once_with(
+            real_data, synthetic_data, metadata)
+        corr_sim_mock.compute_breakdown.assert_called_once_with(
+            real_data, synthetic_data, metadata)
+        cont_sim_mock.compute_breakdown.assert_called_once_with(
+            real_data, synthetic_data, metadata)
+        cardinality_mock.compute_breakdown.assert_called_once_with(
+            real_data, synthetic_data, metadata)
+
+        assert report._overall_quality_score == 0.425
+        assert report._property_breakdown == {
+            'Column Shapes': 0.125,
+            'Column Pair Trends': 0.15000000000000002,
+            'Parent Child Relationships': 1.0,
+        }
+        assert report._property_errors == {
+            'Column Shapes': 1,
+            'Column Pair Trends': 0,
+            'Parent Child Relationships': 0,
         }
 
     @patch('sdmetrics.reports.multi_table.quality_report.discretize_and_apply_metric')
@@ -671,6 +788,63 @@ class TestQualityReport:
             })
         )
 
+    def test_get_details_column_shapes_with_error(self):
+        """Test the ``get_details`` method with column shapes with error.
+
+        Expect that the details of the desired property is returned, and that the details
+        contains an Error column.
+
+        Input:
+        - property name
+
+        Output:
+        - score details for the desired property
+        """
+        # Setup
+        report = QualityReport()
+        report._metric_results = {
+            'KSComplement': {
+                'table1': {
+                    'col1': {'score': 0.1},
+                    'col2': {'error': 'test error'},
+                },
+                'table2': {
+                    'col3': {'score': 0.5},
+                },
+            },
+            'TVComplement': {
+                'table1': {
+                    'col1': {'score': 0.3},
+                    'col2': {'score': 0.4},
+                },
+                'table2': {
+                    'col3': {'score': 0.6},
+                },
+            }
+        }
+
+        # Run
+        out = report.get_details('Column Shapes')
+
+        # Assert
+        pd.testing.assert_frame_equal(
+            out,
+            pd.DataFrame({
+                'Table': ['table1', 'table1', 'table1', 'table1', 'table2', 'table2'],
+                'Column': ['col1', 'col2', 'col1', 'col2', 'col3', 'col3'],
+                'Metric': [
+                    'KSComplement',
+                    'KSComplement',
+                    'TVComplement',
+                    'TVComplement',
+                    'KSComplement',
+                    'TVComplement',
+                ],
+                'Quality Score': [0.1, np.nan, 0.3, 0.4, 0.5, 0.6],
+                'Error': [np.nan, 'test error', np.nan, np.nan, np.nan, np.nan],
+            })
+        )
+
     def test_get_details_column_pair_trends(self):
         """Test the ``get_details`` method with column pair trends.
 
@@ -755,6 +929,47 @@ class TestQualityReport:
             })
         )
 
+    def test_get_details_parent_child_relationships_with_error(self):
+        """Test the ``get_details`` method with parent child relationships with error.
+
+        Expect that the details of the desired property is returned, and that the
+        details contains an Error column.
+
+        Input:
+        - property name
+
+        Output:
+        - score details for the desired property
+        """
+        # Setup
+        report = QualityReport()
+        report._metric_results = {
+            'CardinalityShapeSimilarity': {
+                ('table1', 'table2'): {'score': 0.1},
+                ('table1', 'table3'): {'score': 0.2},
+                ('table1', 'table4'): {'error': 'test error'},
+            },
+        }
+
+        # Run
+        out = report.get_details('Parent Child Relationships')
+
+        # Assert
+        pd.testing.assert_frame_equal(
+            out,
+            pd.DataFrame({
+                'Child Table': ['table2', 'table3', 'table4'],
+                'Parent Table': ['table1', 'table1', 'table1'],
+                'Metric': [
+                    'CardinalityShapeSimilarity',
+                    'CardinalityShapeSimilarity',
+                    'CardinalityShapeSimilarity',
+                ],
+                'Quality Score': [0.1, 0.2, np.nan],
+                'Error': [np.nan, np.nan, 'test error'],
+            })
+        )
+
     def test_get_details_parent_child_relationships_with_table_name(self):
         """Test the ``get_details`` method with parent child relationships with a table filter.
 
@@ -832,3 +1047,117 @@ class TestQualityReport:
                 }
             }
         ]
+
+    def test__print_result(self):
+        """Test the ``_print_results`` method.
+
+        Expect that the correct messages are written.
+
+        Input:
+        - out argument
+
+        Side Effects:
+        - messages are written to the output.
+        """
+        # Setup
+        report = QualityReport()
+        report._overall_quality_score = 0.8
+        report._property_breakdown = {
+            'Column Shapes': 0.6,
+            'Column Pair Trends': 0.8,
+            'Parent Child Relationships': 0.9
+        }
+        report._property_errors = {
+            'Column Shapes': 0,
+            'Column Pair Trends': 0,
+            'Parent Child Relationships': 0,
+        }
+        mock_out = Mock()
+
+        # Run
+        report._print_results(mock_out)
+
+        # Assert
+        mock_out.write.assert_has_calls([
+            call('\nOverall Quality Score: 80.0%\n\n'),
+            call('Properties:\n'),
+            call('Column Shapes: 60.0%\n'),
+            call('Column Pair Trends: 80.0%\n'),
+            call('Parent Child Relationships: 90.0%\n'),
+        ])
+
+    def test__print_result_with_error(self):
+        """Test the ``_print_results`` method with errors.
+
+        Expect that the correct messages are written.
+
+        Input:
+        - out argument
+
+        Side Effects:
+        - messages are written to the output.
+        """
+        # Setup
+        report = QualityReport()
+        report._overall_quality_score = 0.7
+        report._property_breakdown = {
+            'Column Shapes': 0.6,
+            'Column Pair Trends': np.nan,
+            'Parent Child Relationships': 0.8,
+        }
+        report._property_errors = {
+            'Column Shapes': 0,
+            'Column Pair Trends': 1,
+            'Parent Child Relationships': 0,
+        }
+        mock_out = Mock()
+
+        # Run
+        report._print_results(mock_out)
+
+        # Assert
+        mock_out.write.assert_has_calls([
+            call('\nOverall Quality Score: 70.0%\n\n'),
+            call('Properties:\n'),
+            call('Column Shapes: 60.0%\n'),
+            call('Column Pair Trends: Error computing property.\n'),
+            call('Parent Child Relationships: 80.0%\n'),
+        ])
+
+    def test__print_result_with_all_errors(self):
+        """Test the ``_print_results`` method with all properties erroring out.
+
+        Expect that the correct messages are written.
+
+        Input:
+        - out argument
+
+        Side Effects:
+        - messages are written to the output.
+        """
+        # Setup
+        report = QualityReport()
+        report._overall_quality_score = np.nan
+        report._property_breakdown = {
+            'Column Shapes': np.nan,
+            'Column Pair Trends': np.nan,
+            'Parent Child Relationships': np.nan
+        }
+        report._property_errors = {
+            'Column Shapes': 1,
+            'Column Pair Trends': 1,
+            'Parent Child Relationships': 1,
+        }
+        mock_out = Mock()
+
+        # Run
+        report._print_results(mock_out)
+
+        # Assert
+        mock_out.write.assert_has_calls([
+            call('\nOverall Quality Score: Error computing report.\n\n'),
+            call('Properties:\n'),
+            call('Column Shapes: Error computing property.\n'),
+            call('Column Pair Trends: Error computing property.\n'),
+            call('Parent Child Relationships: Error computing property.\n'),
+        ])
