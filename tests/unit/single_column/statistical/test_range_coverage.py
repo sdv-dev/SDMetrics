f895c7748ce91e17afdb210cb7dea49f96f95fd4
Katharine Xiao <2405771+katxiao@users.noreply.github.com>
2022-09-14 17:09:09 -0400

    Add unit test

--- tests/unit/single_column/statistical/test_range_coverage.py
+++ tests/unit/single_column/statistical/test_range_coverage.py
@@ -1,5 +1,6 @@
 from unittest.mock import patch
 
+import numpy as np
 import pandas as pd
 
 from sdmetrics.single_column.statistical import RangeCoverage
@@ -30,6 +31,30 @@ class TestRangeCoverage:
 
         # Assert
         assert result == 0.5
+
+    def test_compute_constant_input(self):
+        """Test the ``compute`` method with constant input.
+
+        Expect that the range coverage is returned.
+
+        Input:
+        - Real data.
+        - Synthetic data.
+
+        Output:
+        - The evaluated metric.
+        """
+        # Setup
+        real_data = pd.Series([1, 1, 1, 1, 1])
+        synthetic_data = pd.Series([2, 3, 1, 2, 3])
+
+        metric = RangeCoverage()
+
+        # Run
+        result = metric.compute(real_data, synthetic_data)
+
+        # Assert
+        assert np.isnan(result)
 
     @patch('sdmetrics.single_column.statistical.range_coverage.SingleColumnMetric.normalize')
     def test_normalize(self, normalize_mock):
