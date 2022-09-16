0899c67fc04ff60c495f12e860b9bd12ab89eb94
Katharine Xiao <2405771+katxiao@users.noreply.github.com>
2022-09-16 15:02:52 -0400

    Save version (#229)
    * Save version for single table quality report
    
    * Save version for multi table quality report
    
    * address cr

--- tests/unit/reports/multi_table/test_multi_table_quality_report.py
+++ tests/unit/reports/multi_table/test_multi_table_quality_report.py
@@ -529,8 +529,9 @@ class TestQualityReport:
             }),
         )
 
+    @patch('sdmetrics.reports.multi_table.quality_report.pkg_resources.get_distribution')
     @patch('sdmetrics.reports.multi_table.quality_report.pickle')
-    def test_save(self, pickle_mock):
+    def test_save(self, pickle_mock, get_distribution_mock):
         """Test the ``save`` method.
 
         Expect that the instance is passed to pickle.
@@ -550,11 +551,14 @@ class TestQualityReport:
             QualityReport.save(report, 'test-file.pkl')
 
         # Assert
+        get_distribution_mock.assert_called_once_with('sdmetrics')
         open_mock.assert_called_once_with('test-file.pkl', 'wb')
         pickle_mock.dump.assert_called_once_with(report, open_mock())
+        assert report._package_version == get_distribution_mock.return_value.version
 
+    @patch('sdmetrics.reports.multi_table.quality_report.pkg_resources.get_distribution')
     @patch('sdmetrics.reports.multi_table.quality_report.pickle')
-    def test_load(self, pickle_mock):
+    def test_load(self, pickle_mock, get_distribution_mock):
         """Test the ``load`` method.
 
         Expect that the report's load method is called with the expected args.
@@ -570,6 +574,9 @@ class TestQualityReport:
         """
         # Setup
         open_mock = mock_open(read_data=pickle.dumps('test'))
+        report = Mock()
+        pickle_mock.load.return_value = report
+        report._package_version = get_distribution_mock.return_value.version
 
         # Run
         with patch('sdmetrics.reports.multi_table.quality_report.open', open_mock):
@@ -577,6 +584,43 @@ class TestQualityReport:
 
         # Assert
         open_mock.assert_called_once_with('test-file.pkl', 'rb')
+        assert loaded == pickle_mock.load.return_value
+
+    @patch('sdmetrics.reports.multi_table.quality_report.warnings')
+    @patch('sdmetrics.reports.multi_table.quality_report.pkg_resources.get_distribution')
+    @patch('sdmetrics.reports.multi_table.quality_report.pickle')
+    def test_load_mismatched_versions(self, pickle_mock, get_distribution_mock, warnings_mock):
+        """Test the ``load`` method with mismatched sdmetrics versions.
+
+        Expect that the report's load method is called with the expected args.
+        Expect that a warning is raised.
+
+        Input:
+        - filepath
+
+        Output:
+        - the loaded model
+
+        Side Effects:
+        - Expect that ``pickle`` is called with the filepath.
+        """
+        # Setup
+        open_mock = mock_open(read_data=pickle.dumps('test'))
+        report = Mock()
+        pickle_mock.load.return_value = report
+        report._package_version = 'previous_version'
+        get_distribution_mock.return_value.version = 'new_version'
+
+        # Run
+        with patch('sdmetrics.reports.multi_table.quality_report.open', open_mock):
+            loaded = QualityReport.load('test-file.pkl')
+
+        # Assert
+        open_mock.assert_called_once_with('test-file.pkl', 'rb')
+        warnings_mock.warn.assert_called_once_with(
+            'The report was created using SDMetrics version `previous_version` but you are '
+            'currently using version `new_version`. Some features may not work as intended.'
+        )
         assert loaded == pickle_mock.load.return_value
 
     @patch('sdmetrics.reports.multi_table.quality_report.get_column_shapes_plot')
