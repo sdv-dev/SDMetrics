81e7ca572ce7baf61beb41e13617ce8f8d5fbf07
Katharine Xiao <2405771+katxiao@users.noreply.github.com>
2022-11-10 12:50:13 -0500

    Fix range coverage with nan data (#256)

--- tests/unit/single_column/statistical/test_range_coverage.py
+++ tests/unit/single_column/statistical/test_range_coverage.py
@@ -56,6 +56,40 @@ class TestRangeCoverage:
         # Assert
         assert np.isnan(result)
 
+    def test_compute_with_nans(self):
+        """Test the ``compute`` method with NaN values in the input data.
+
+        Expect that the range coverage is returned.
+        """
+        # Setup
+        real_data = pd.Series([1, np.nan, 3, 4, 5])
+        synthetic_data = pd.Series([2, 3, 1, np.nan, 3])
+
+        metric = RangeCoverage()
+
+        # Run
+        result = metric.compute(real_data, synthetic_data)
+
+        # Assert
+        assert result == 0.5
+
+    def test_compute_with_list_input(self):
+        """Test the ``compute`` method with list input.
+
+        Expect that the range coverage is returned.
+        """
+        # Setup
+        real_data = [1, np.nan, 3, 4, 5]
+        synthetic_data = [2, 3, 1, np.nan, 3]
+
+        metric = RangeCoverage()
+
+        # Run
+        result = metric.compute(real_data, synthetic_data)
+
+        # Assert
+        assert result == 0.5
+
     @patch('sdmetrics.single_column.statistical.range_coverage.SingleColumnMetric.normalize')
     def test_normalize(self, normalize_mock):
         """Test the ``normalize`` method.
