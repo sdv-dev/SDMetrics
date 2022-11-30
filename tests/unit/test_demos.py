1e9fd4bfd9bb78cfa2780634bf66a95bec5ebfed
Katharine Xiao <2405771+katxiao@users.noreply.github.com>
2022-11-30 13:18:55 -0500

    update transactions column

--- tests/unit/test_demos.py
+++ tests/unit/test_demos.py
@@ -13,3 +13,20 @@ def test_load_single_table_demo():
     assert metadata['fields']['duration'] == {'type': 'numerical', 'subtype': 'integer'}
     assert real_data['duration'].dtype == 'float64'
     assert synthetic_data['duration'].dtype == 'float64'
+
+
+def test_load_multi_table_demo():
+    """Test loading the multi table demo data and expect the correct demo data to be returned."""
+    # Setup
+    modality = 'multi_table'
+
+    # Run
+    real_data, synthetic_data, metadata = load_demo(modality)
+
+    # Assert
+    assert metadata['tables']['transactions']['fields']['timestamp'] == {
+        'type': 'datetime',
+        'format': '%Y-%m-%d %H:%M:%S',
+    }
+    assert real_data['transactions']['timestamp'].dtype == 'datetime64[ns]'
+    assert synthetic_data['transactions']['timestamp'].dtype == 'datetime64[ns]'
