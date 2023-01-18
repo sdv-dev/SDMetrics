c9967494126e6273d3d97ebf8c1b045861a3f126
Katharine Xiao <2405771+katxiao@users.noreply.github.com>
2023-01-18 15:58:48 -0500

    Bump version: 0.9.0 → 0.9.1.dev0

--- setup.py
+++ setup.py
@@ -125,6 +125,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/SDMetrics',
-    version='0.9.0',
+    version='0.9.1.dev0',
     zip_safe=False,
 )
