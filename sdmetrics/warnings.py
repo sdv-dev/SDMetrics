1d7a09c54522c41607f1391feaa7d61145cae54d
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2023-03-22 18:15:53 +0100

    Add more detailed warnings for the QualityReport

--- sdmetrics/warnings.py
+++ sdmetrics/warnings.py
@@ -1,7 +1,11 @@
 """Warnings for sdmetrics."""
 
+class SDMetricsWarning(RuntimeWarning):
+    """Class to represent SDMetrics warnings."""
+    pass
 
-class ConstantInputWarning(RuntimeWarning):
+
+class ConstantInputWarning(SDMetricsWarning):
     """Thrown when the input data has all the same values."""
 
     def __init__(self, message):
