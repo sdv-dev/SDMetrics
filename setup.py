c25f0f1dbe23983d6a241b1a40ffdc61145cd756
Frances Hartwell <frances.hartwell@alteryx.com>
2023-03-29 18:01:22 -0400

    fix minimum tests

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ install_requires = [
     'copulas>=0.8.0,<0.9',
     'tqdm>=4.15,<5',
     'plotly>=5.10.0,<6',
-    "importlib-metadata>=4.6;python_version<'3.10'"
+    "importlib-metadata>=4.6,<6;python_version<'3.10'"
 ]
 
 pomegranate_requires = [
