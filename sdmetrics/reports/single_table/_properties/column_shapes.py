75035fd1d7916d737dcbf4ae2d81ceae606092bf
R-Palazzo <romainpalazzo@gmail.com>
2023-06-11 11:41:48 -0400

    get_visualization 1

--- sdmetrics/reports/single_table/_properties/column_shapes.py
+++ sdmetrics/reports/single_table/_properties/column_shapes.py
@@ -3,6 +3,8 @@ from sdmetrics.single_column.statistical.kscomplement import KSComplement
 from sdmetrics.single_column.statistical.tv_complement import TVComplement
 import pandas as pd
 import numpy as np
+import plotly.express as px
+
 
 class ColumnShapes(BaseSingleTableProperty):
     
@@ -37,9 +39,49 @@ class ColumnShapes(BaseSingleTableProperty):
             scores.append(column_score)
     
         result = pd.DataFrame({
-            'Column': column_names,
+            'Column name': column_names,
             'Metric': metric_names,
             'Score': scores,
         })
 
         return result
+
+    def get_visualization(self):
+        """Create a plot to show the column shape scores.
+
+        Returns:
+            plotly.graph_objects._figure.Figure
+        """
+        average_score = self._average_scores()
+        data = self._details
+    
+        fig = px.bar(
+            data,
+            x='Column name',
+            y='Score',
+            title=f'Data Quality: Column Shapes (Average Score={round(average_score, 2)})',
+            category_orders={'group': data['Column Name']},
+            color='Metric',
+            color_discrete_map={
+                'KSComplement': '#000036',
+                'TVComplement': '#03AFF1',
+            },
+            pattern_shape='Metric',
+            pattern_shape_sequence=['', '/'],
+            hover_name='Column Name',
+            hover_data={
+                'Column Name': False,
+                'Metric': True,
+                'Quality Score': True,
+            },
+        )
+
+        fig.update_yaxes(range=[0, 1])
+
+        fig.update_layout(
+            xaxis_categoryorder='total ascending',
+            plot_bgcolor='#F5F5F8',
+            margin={'t': 150},
+        )
+
+        return fig
