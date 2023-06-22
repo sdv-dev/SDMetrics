a5d06d72be1450de4780cfbe51c7e6977ede6882
R-Palazzo <romainpalazzo@gmail.com>
2023-06-22 15:00:03 +0100

    fix integration

--- tests/integration/reports/single_table/_properties/test_column_pair_trends.py
+++ tests/integration/reports/single_table/_properties/test_column_pair_trends.py
@@ -1,9 +1,8 @@
-import json
-import re
 
 import numpy as np
 import pandas as pd
 
+from sdmetrics.demos import load_demo
 from sdmetrics.reports.single_table._properties.column_pair_trends import ColumnPairTrends
 
 
@@ -18,10 +17,7 @@ class TestColumnPairTrends:
         column_names = [
             'student_id', 'degree_type', 'start_date', 'second_perc', 'work_experience'
         ]
-        real_data = pd.read_csv('sdmetrics/demos/single_table/real.csv')[column_names]
-        synthetic_data = pd.read_csv('sdmetrics/demos/single_table/synthetic.csv')[column_names]
-        with open('sdmetrics/demos/single_table/metadata.json', 'r') as f:
-            metadata = json.load(f)
+        real_data, synthetic_data, metadata = load_demo(modality='single_table')
 
         metadata['columns'] = {
             key: val for key, val in metadata['columns'].items() if key in column_names
@@ -46,8 +42,8 @@ class TestColumnPairTrends:
                 'ContingencySimilarity', 'ContingencySimilarity', 'ContingencySimilarity'
             ],
             'Score': [
-                0.9854510263003199, 0.8, 0.8511627906976744, 0.627906976744186,
-                0.6139534883720931, 0.8976744186046511
+                0.9854510263003199, 0.586046511627907, 0.6232558139534884, 0.7348837209302326,
+                0.6976744186046512, 0.8976744186046511
             ],
             'Real Correlation': [
                 0.04735340044317632, np.nan, np.nan, np.nan, np.nan, np.nan
@@ -58,7 +54,7 @@ class TestColumnPairTrends:
         }
         expected_details = pd.DataFrame(expected_details_dict)
         pd.testing.assert_frame_equal(column_shape_property._details, expected_details)
-        assert score == 0.796
+        assert score == 0.754
 
     def test_get_score_warnings(self, recwarn):
         """Test the ``get_score`` method when the metrics are raising erros for some columns."""
@@ -66,10 +62,7 @@ class TestColumnPairTrends:
         column_names = [
             'student_id', 'degree_type', 'start_date', 'second_perc', 'work_experience'
         ]
-        real_data = pd.read_csv('sdmetrics/demos/single_table/real.csv')[column_names]
-        synthetic_data = pd.read_csv('sdmetrics/demos/single_table/synthetic.csv')[column_names]
-        with open('sdmetrics/demos/single_table/metadata.json', 'r') as f:
-            metadata = json.load(f)
+        real_data, synthetic_data, metadata = load_demo(modality='single_table')
 
         metadata['columns'] = {
             key: val for key, val in metadata['columns'].items() if key in column_names
@@ -80,24 +73,21 @@ class TestColumnPairTrends:
         # Run
         column_shape_property = ColumnPairTrends()
 
-        expected_message_1 = re.escape(
-            "Unable to discretize 'second_perc'. No column pair trends metric will be "
-            'calculated between this column and boolean/categorical columns. Encountered '
-            'Error: ValueError Unable to parse string \"a\" at position 2'
+        exp_message_1 = (
+            "Error: ValueError could not convert string to float: 'a'"
         )
 
-        expected_message_2 = re.escape(
-            "Unable to compute Column Pair Trends for column ('start_date', 'second_perc'). "
-            "Encountered Error: ValueError could not convert string to float: 'a'"
+        exp_message_2 = (
+            "Error: TypeError '<=' not supported between instances of 'float' and 'str'"
+        )
+
+        exp_error_serie = pd.Series(
+            [exp_message_1, None, None, exp_message_2, exp_message_2, None]
         )
 
         score = column_shape_property.get_score(real_data, synthetic_data, metadata)
 
         # Assert
-        assert re.match(expected_message_1, str(recwarn[0].message))
-        assert re.match(expected_message_2, str(recwarn[1].message))
-
         details = column_shape_property._details
-        column_names_nan = list(details.loc[pd.isna(details['Score'])]['Column 2'])
-        assert column_names_nan == ['second_perc']
-        assert score == 0.51
+        pd.testing.assert_series_equal(details['Error'], exp_error_serie, check_names=False)
+        assert score == 0.702
