4f398750dcd5255f18212cc4e4a245302c2d949a
R-Palazzo <romainpalazzo@gmail.com>
2023-06-30 11:41:42 +0100

    rebase init

--- sdmetrics/reports/multi_table/_properties/__init__.py
+++ sdmetrics/reports/multi_table/_properties/__init__.py
@@ -2,6 +2,8 @@
 
 from sdmetrics.reports.multi_table._properties.base import BaseMultiTableProperty
 from sdmetrics.reports.multi_table._properties.cardinality import Cardinality
+from sdmetrics.reports.multi_table._properties.column_pair_trends import ColumnPairTrends
+from sdmetrics.reports.multi_table._properties.column_shapes import ColumnShapes
 
 __all__ = [
     'BaseMultiTableProperty',
