d18af0b1a4e7ba8141ba408f3f75e62fbe038a70
R-Palazzo <romainpalazzo@gmail.com>
2023-07-20 15:31:03 +0100

    modify integration test

--- tests/integration/reports/single_table/_properties/test_synthesis.py
+++ tests/integration/reports/single_table/_properties/test_synthesis.py
@@ -20,14 +20,16 @@ class TestSynthesis:
         assert score == 0.5
 
     def test_get_score_error(self):
-        """Test the ``get_score`` method with an error."""
+        """Test the ``get_score`` method with an error.
+
+        Give an empty synthetic data to get an error.
+        """
         # Setup
         real_data, _, metadata = load_demo('single_table')
-        real_data['start_date'].iloc[0] = 'Err'
 
         # Run
         synthesis_property = Synthesis()
-        score = synthesis_property.get_score(real_data.iloc[:20], real_data.iloc[10:30], metadata)
+        score = synthesis_property.get_score(real_data.iloc[:20], [], metadata)
 
         # Assert
         assert pd.isna(score)
@@ -37,7 +39,7 @@ class TestSynthesis:
             'Score': np.nan,
             'Num Matched Rows': np.nan,
             'Num New Rows': np.nan,
-            'Error': 'Error: ValueError "Timestamp" is not a supported function'
+            'Error': "Error: AttributeError 'list' object has no attribute 'columns'"
         }, index=[0])
 
         pd.testing.assert_frame_equal(synthesis_property._details, expected_details)
