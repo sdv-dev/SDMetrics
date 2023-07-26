fd95051980a0d97853c7a2a3fab417b42ffd7887
R-Palazzo <116157184+R-Palazzo@users.noreply.github.com>
2023-07-26 10:05:43 +0100

    Create single table coverage property (#395)
    * definition
    
    * unit tests
    
    * integration test
    
    * move integration test
    
    * typo
    
    * typo 2
    
    * docstring

--- sdmetrics/reports/single_table/_properties/__init__.py
+++ sdmetrics/reports/single_table/_properties/__init__.py
@@ -3,9 +3,11 @@
 from sdmetrics.reports.single_table._properties.base import BaseSingleTableProperty
 from sdmetrics.reports.single_table._properties.column_pair_trends import ColumnPairTrends
 from sdmetrics.reports.single_table._properties.column_shapes import ColumnShapes
+from sdmetrics.reports.single_table._properties.coverage import Coverage
 
 __all__ = [
     'BaseSingleTableProperty',
     'ColumnShapes',
     'ColumnPairTrends',
+    'Coverage',
 ]
