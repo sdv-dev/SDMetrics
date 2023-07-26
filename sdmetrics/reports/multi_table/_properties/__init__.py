bab9c61806f49b782d7cb478f4219930dc0ca64d
R-Palazzo <romainpalazzo@gmail.com>
2023-07-26 10:45:29 +0100

    coverage

--- sdmetrics/reports/multi_table/_properties/__init__.py
+++ sdmetrics/reports/multi_table/_properties/__init__.py
@@ -4,10 +4,12 @@ from sdmetrics.reports.multi_table._properties.base import BaseMultiTablePropert
 from sdmetrics.reports.multi_table._properties.cardinality import Cardinality
 from sdmetrics.reports.multi_table._properties.column_pair_trends import ColumnPairTrends
 from sdmetrics.reports.multi_table._properties.column_shapes import ColumnShapes
+from sdmetrics.reports.multi_table._properties.coverage import Coverage
 
 __all__ = [
     'BaseMultiTableProperty',
     'Cardinality',
     'ColumnShapes',
-    'ColumnPairTrends'
+    'ColumnPairTrends',
+    'Coverage'
 ]
--- tests/integration/reports/multi_table/_properties/test_multi_table_properties.py
+++ tests/integration/reports/multi_table/_properties/test_multi_table_properties.py
@@ -3,7 +3,7 @@ from unittest.mock import Mock
 from tqdm import tqdm
 
 from sdmetrics.demos import load_demo
-from sdmetrics.reports.multi_table._properties import ColumnPairTrends, ColumnShapes
+from sdmetrics.reports.multi_table._properties import ColumnPairTrends, ColumnShapes, Coverage
 
 
 def test_column_shapes_property():
@@ -20,6 +20,7 @@ def test_column_shapes_property():
 
 
 def test_column_shapes_property_with_progress_bar():
+    """Test that the progress bar is correctly updated."""
     # Setup
     real_data, synthetic_data, metadata = load_demo(modality='multi_table')
     column_shapes = ColumnShapes()
@@ -47,4 +48,35 @@ def test_column_pair_trends_property():
     result = column_pair_trends.get_score(real_data, synthetic_data, metadata)
 
     # Assert
-    assert result == 0.49349249665827416
+    assert result == 0.49
+
+
+def test_coverage_property():
+    """Test Coverage multi-table class."""
+    # Setup
+    real_data, synthetic_data, metadata = load_demo(modality='multi_table')
+    coverage = Coverage()
+
+    # Run
+    result = coverage.get_score(real_data, synthetic_data, metadata)
+
+    # Assert
+    assert result == 0.96
+
+
+def test_coverage_property_with_progress_bar():
+    # Setup
+    real_data, synthetic_data, metadata = load_demo(modality='multi_table')
+    coverage = Coverage()
+    num_columns = sum(len(table['columns']) for table in metadata['tables'].values())
+
+    progress_bar = tqdm(total=num_columns)
+    mock_update = Mock()
+    progress_bar.update = mock_update
+
+    # Run
+    result = coverage.get_score(real_data, synthetic_data, metadata, progress_bar)
+
+    # Assert
+    assert result == 0.96
+    assert mock_update.call_count == num_columns
