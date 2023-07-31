f45b494f2a2dc7c7d3750772e26465d4ded02a57
R-Palazzo <116157184+R-Palazzo@users.noreply.github.com>
2023-07-31 09:41:54 +0100

    Issue 393 multi table properties (#409)
    * coverage
    
    * boundary
    
    * synthesis
    
    * nanmean
    
    * moving integration tests
    
    * rebase

---
+++ tests/unit/reports/multi_table/_properties/test_synthesis.py
@@ -0,0 +1,13 @@
+"""Test Synthesis multi-table class."""
+from sdmetrics.reports.multi_table._properties import Synthesis
+from sdmetrics.reports.single_table._properties import Synthesis as SingleTableSynthesis
+
+
+def test__init__():
+    """Test the ``__init__`` method."""
+    # Setup
+    synthesis = Synthesis()
+
+    # Assert
+    assert synthesis._properties == {}
+    assert synthesis._single_table_property == SingleTableSynthesis
