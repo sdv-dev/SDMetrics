f188d8a0084a17725bb17ecc01fbdd1a01c6ba9d
Felipe <fealho@gmail.com>
2023-08-03 06:40:39 -0700

    Add errors

--- sdmetrics/column_pairs/statistical/correlation_similarity.py
+++ sdmetrics/column_pairs/statistical/correlation_similarity.py
@@ -9,7 +9,7 @@ from scipy.stats import pearsonr, spearmanr
 from sdmetrics.column_pairs.base import ColumnPairsMetric
 from sdmetrics.goal import Goal
 from sdmetrics.utils import is_datetime
-from sdmetrics.warnings import ConstantInputWarning
+from sdmetrics.errors import ConstantInputError
 
 
 class CorrelationSimilarity(ColumnPairsMetric):
@@ -32,19 +32,28 @@ class CorrelationSimilarity(ColumnPairsMetric):
     max_value = 1.0
 
     @staticmethod
-    def _generate_warning_msg(columns, prefix, warning_messages):
+    def _raise_constant_data_error(columns, prefix):
         if len(columns) > 1:
             cols = ', '.join(columns)
-            warning_messages.append(
+            raise ConstantInputError(
                 f"The {prefix} in columns '{cols}' contain a constant value. "
                 'Correlation is undefined for constant data.'
             )
 
         elif len(columns):
-            warning_messages.append(
+            raise ConstantInputError(
                 f"The {prefix} in column '{columns[0]}' contains a constant value. "
                 'Correlation is undefined for constant data.'
             )
+    
+    @classmethod
+    def _validate_data_not_constant(cls, real_data, synthetic_data):
+        if (real_data.nunique() == 1).any() or (synthetic_data.nunique() == 1).any():
+            real_columns = list(real_data.loc[:, real_data.nunique() == 1].columns)
+            cls._validate_data_not_constant(real_columns, 'real data')
+
+            synthetic_columns = list(synthetic_data.loc[:, synthetic_data.nunique() == 1].columns)
+            cls._validate_data_not_constant(synthetic_columns, 'synthetic data')
 
     @classmethod
     def compute_breakdown(cls, real_data, synthetic_data, coefficient='Pearson'):
@@ -67,17 +76,7 @@ class CorrelationSimilarity(ColumnPairsMetric):
             real_data = pd.DataFrame(real_data)
             synthetic_data = pd.DataFrame(synthetic_data)
 
-        if (real_data.nunique() == 1).any() or (synthetic_data.nunique() == 1).any():
-            warning_messages = []
-            real_columns = list(real_data.loc[:, real_data.nunique() == 1].columns)
-            synthetic_columns = list(synthetic_data.loc[:, synthetic_data.nunique() == 1].columns)
-            cls._generate_warning_msg(real_columns, 'real data', warning_messages)
-            cls._generate_warning_msg(synthetic_columns, 'synthetic data', warning_messages)
-
-            for msg in warning_messages:
-                warnings.warn(ConstantInputWarning(msg))
-
-            return {'score': np.nan}
+        cls._validate_data_not_constant(real_data, synthetic_data)
 
         column1, column2 = real_data.columns[:2]
         real_data = real_data[[column1, column2]].dropna()
--- sdmetrics/errors.py
+++ sdmetrics/errors.py
@@ -3,3 +3,6 @@
 
 class IncomputableMetricError(Exception):
     """Raised when a metric cannot be computed."""
+
+class ConstantInputError(Exception):
+    """Thrown when the input data has all the same values."""
