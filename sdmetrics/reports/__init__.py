6fa8b31ab220c7baf67fb2414c9f3bf8075d2580
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2023-09-25 11:43:54 +0200

    Remove get_column_plot from reports utils

--- sdmetrics/reports/__init__.py
+++ sdmetrics/reports/__init__.py
@@ -1,7 +1,6 @@
 """Reports for sdmetrics."""
-from sdmetrics.reports.utils import get_column_pair_plot, get_column_plot
+from sdmetrics.reports.utils import get_column_pair_plot
 
 __all__ = [
     'get_column_pair_plot',
-    'get_column_plot',
 ]
