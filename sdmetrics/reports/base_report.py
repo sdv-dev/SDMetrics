818e9c49a54b8dadb903adb8ff229c8b027cc471
R-Palazzo <romainpalazzo@gmail.com>
2023-10-12 14:44:48 +0200

    def

--- sdmetrics/reports/base_report.py
+++ sdmetrics/reports/base_report.py
@@ -55,13 +55,24 @@ class BaseReport():
             metadata (dict):
                 The metadata of the table.
         """
-        if not isinstance(metadata, dict):
-            metadata = metadata.to_dict()
-
         self._validate_metadata_matches_data(real_data, synthetic_data, metadata)
 
     def _handle_results(self, verbose):
         raise NotImplementedError
+
+    @staticmethod
+    def _convert_metadata(metadata):
+        """If the metadta is not a dict, try to convert it."""
+        if not isinstance(metadata, dict):
+            try:
+                metadata = metadata.to_dict()
+            except Exception:
+                raise TypeError(
+                    'The provided metadata is not a dictionary and does not have a to_dict method.'
+                    'Please convert the metadata to a dictionary.'
+                )
+
+        return metadata
 
     @staticmethod
     def convert_datetimes(real_data, synthetic_data, metadata):
@@ -101,6 +112,7 @@ class BaseReport():
             verbose (bool):
                 Whether or not to print report summary and progress.
         """
+        metadata = self._convert_metadata(metadata)
         self.validate(real_data, synthetic_data, metadata)
         self.convert_datetimes(real_data, synthetic_data, metadata)
 
