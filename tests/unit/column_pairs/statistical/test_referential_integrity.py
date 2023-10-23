bf7709e8beedfd35fa18c27c24368c85716d35d7
R-Palazzo <romainpalazzo@gmail.com>
2023-10-23 08:03:40 -0600

    def + test

---
+++ tests/unit/column_pairs/statistical/test_referential_integrity.py
@@ -0,0 +1,87 @@
+from unittest.mock import patch
+
+import pandas as pd
+
+from sdmetrics.column_pairs.statistical import ReferentialIntegrity
+
+
+class TestReferentialIntegrity:
+
+    def test_compute_breakdown(self):
+        """Test the ``compute_breakdown`` method."""
+        # Setup
+        real_data = pd.DataFrame({
+            'primary_key': [1, 2, 3, 4, 5],
+            'foreign_key': [1, 2, 3, 2, 1]
+        })
+        synthetic_data = pd.DataFrame({
+            'primary_key': [1, 2, 3, 4, 5],
+            'foreign_key': [1, 6, 3, 4, 5]
+        })
+
+        metric = ReferentialIntegrity()
+        tuple_real = (real_data['primary_key'], real_data['foreign_key'])
+        tuple_synthetic = (synthetic_data['primary_key'], synthetic_data['foreign_key'])
+
+        # Run
+        result = metric.compute_breakdown(tuple_real, tuple_synthetic)
+
+        # Assert
+        assert result == {'score': 0.8}
+
+    @patch('sdmetrics.column_pairs.statistical.referential_integrity.LOGGER')
+    def test_compute_breakdown_with_missing_relations_real_data(self, logger_mock):
+        """Test the ``compute_breakdown`` when there is missing relationships in the real data."""
+        # Setup
+        real_data = pd.DataFrame({
+            'primary_key': [1, 2, 3, 4, 5],
+            'foreign_key': [1, 2, 6, 2, 1]
+        })
+        synthetic_data = pd.DataFrame({
+            'primary_key': [1, 2, 3, 4, 5],
+            'foreign_key': [1, 6, 3, 4, 5]
+        })
+
+        metric = ReferentialIntegrity()
+        tuple_real = (real_data['primary_key'], real_data['foreign_key'])
+        tuple_synthetic = (synthetic_data['primary_key'], synthetic_data['foreign_key'])
+
+        # Run
+        result = metric.compute_breakdown(tuple_real, tuple_synthetic)
+
+        # Assert
+        expected_message = "The real data has foreign keys that don't reference any primary key."
+        assert result == {'score': 0.8}
+        logger_mock.info.assert_called_once_with(expected_message)
+
+    @patch('sdmetrics.column_pairs.statistical.referential_integrity.'
+           'ReferentialIntegrity.compute_breakdown')
+    def test_compute(self, compute_breakdown_mock):
+        """Test the ``compute`` method."""
+        # Setup
+        real_data = pd.Series(['A', 'B', 'C', 'B', 'A'])
+        synthetic_data = pd.Series(['A', 'B', 'C', 'D', 'E'])
+        metric = ReferentialIntegrity()
+        compute_breakdown_mock.return_value = {'score': 0.6}
+
+        # Run
+        result = metric.compute(real_data, synthetic_data)
+
+        # Assert
+        compute_breakdown_mock.assert_called_once_with(real_data, synthetic_data)
+        assert result == 0.6
+
+    @patch('sdmetrics.column_pairs.statistical.referential_integrity.'
+           'ColumnPairsMetric.normalize')
+    def test_normalize(self, normalize_mock):
+        """Test the ``normalize`` method."""
+        # Setup
+        metric = ReferentialIntegrity()
+        raw_score = 0.9
+
+        # Run
+        result = metric.normalize(raw_score)
+
+        # Assert
+        normalize_mock.assert_called_once_with(raw_score)
+        assert result == normalize_mock.return_value
