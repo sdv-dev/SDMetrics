fdd8a77cae6fd6d8800bde60a5d93a4a606486d1
R-Palazzo <romainpalazzo@gmail.com>
2023-10-26 15:16:12 -0600

    lint

--- sdmetrics/reports/multi_table/_properties/data_validity.py
+++ sdmetrics/reports/multi_table/_properties/data_validity.py
@@ -10,8 +10,8 @@ class DataValidity(BaseMultiTableProperty):
     The metric is based on the type data in each column.
     A metric score is computed column-wise and the final score is the average over all columns.
     The BoundaryAdherence metric is used for numerical and datetime columns, the CategoryAdherence
-    is used for categorical and boolean columns and the KeyUniqueness for primary and alternate keys.
-    The other column types are ignored by this property.
+    is used for categorical and boolean columns and the KeyUniqueness for primary and
+    alternate keys. The other column types are ignored by this property.
     """
 
     _single_table_property = SingleTableDataValidity
