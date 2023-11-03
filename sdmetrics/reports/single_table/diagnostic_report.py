08cb584df349fedf85e877b03b197a1cf0db2c55
R-Palazzo <romainpalazzo@gmail.com>
2023-11-03 10:57:58 -0600

    change property

--- sdmetrics/reports/single_table/diagnostic_report.py
+++ sdmetrics/reports/single_table/diagnostic_report.py
@@ -1,10 +1,9 @@
 """Single table diagnostic report."""
 import logging
-from copy import deepcopy
 
-from sdmetrics.reports._results_handler import DiagnosticReportResultsHandler
+from sdmetrics.reports._results_handler import QualityReportResultsHandler
 from sdmetrics.reports.base_report import BaseReport
-from sdmetrics.reports.single_table._properties import Boundary, Coverage, Synthesis
+from sdmetrics.reports.single_table._properties import DataValidity, Structure
 
 LOGGER = logging.getLogger(__name__)
 
@@ -13,22 +12,21 @@ class DiagnosticReport(BaseReport):
     """Single table diagnostic report.
 
     This class creates a diagnostic report for single-table data. It calculates the diagnostic
-    score along three properties - Synthesis, Coverage, and Boundary.
+    score along three properties - Structure, RelationshipValidity, and DataValidity.
     """
 
     def __init__(self):
         super().__init__()
         self._properties = {
-            'Coverage': Coverage(),
-            'Boundary': Boundary(),
-            'Synthesis': Synthesis()
+            'Data Validity': DataValidity(),
+            'Data Structure': Structure(),
         }
-        self._results_handler = DiagnosticReportResultsHandler()
+        self._results_handler = QualityReportResultsHandler()
 
     def _handle_results(self, verbose):
-        self._results_handler.print_results(self._properties, verbose)
+        self._results_handler.print_results(self._properties, self._overall_score, verbose)
 
-    def get_results(self):
+    def get_score(self):
         """Return the diagnostic results.
 
         Returns:
@@ -36,4 +34,4 @@ class DiagnosticReport(BaseReport):
                 The diagnostic results.
         """
         self._check_report_generated()
-        return deepcopy(self._results_handler.results)
+        return self._overall_score
