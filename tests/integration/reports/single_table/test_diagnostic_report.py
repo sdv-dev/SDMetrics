04d709809e156b4053031ac9042bd420cbf51f6c
R-Palazzo <116157184+R-Palazzo@users.noreply.github.com>
2023-11-06 16:27:47 -0600

    Update `DiagnosticReport` to calculate base correctness of synthetic data (#496)

--- tests/integration/reports/single_table/test_diagnostic_report.py
+++ tests/integration/reports/single_table/test_diagnostic_report.py
@@ -20,63 +20,38 @@ class TestDiagnosticReport:
         # Assert
         expected_frame = pd.DataFrame(
             {
-                'Property': ['Coverage', 'Boundary', 'Synthesis'],
-                'Score': [0.9419212095491987, 0.9172655676537751, 1.0]
+                'Property': ['Data Validity', 'Data Structure'],
+                'Score': [0.951333, 1.0]
             }
         )
         pd.testing.assert_frame_equal(properties_frame, expected_frame)
 
-    def test_get_results(self):
-        """Test the ``get_results`` method."""
+    def test_get_score(self):
+        """Test the ``get_score`` method."""
         # Setup
         real_data, synthetic_data, metadata = load_demo(modality='single_table')
         report = DiagnosticReport()
 
         # Run
         report.generate(real_data, synthetic_data, metadata)
-        result_dict = report.get_results()
+        result = report.get_score()
 
         # Assert
-        expected_results = {
-            'SUCCESS': [
-                'The synthetic data covers over 90% of the categories present in the real data',
-                'The synthetic data covers over 90% of the numerical ranges present in the '
-                'real data',
-                'The synthetic data follows over 90% of the min/max boundaries set by the '
-                'real data',
-                'Over 90% of the synthetic rows are not copies of the real data'
-            ],
-            'WARNING': [],
-            'DANGER': []
-        }
 
-        assert result_dict == expected_results
+        assert result == 0.975666343427581
 
-    def test_get_results_with_no_verbose(self):
-        """Test the ``get_results`` method works when verbose=False."""
+    def test_get_score_with_no_verbose(self):
+        """Test the ``get_score`` method works when verbose=False."""
         # Setup
         real_data, synthetic_data, metadata = load_demo(modality='single_table')
         report = DiagnosticReport()
 
         # Run
         report.generate(real_data, synthetic_data, metadata, verbose=False)
-        result_dict = report.get_results()
+        result_dict = report.get_score()
 
         # Assert
-        expected_results = {
-            'SUCCESS': [
-                'The synthetic data covers over 90% of the categories present in the real data',
-                'The synthetic data covers over 90% of the numerical ranges present in the '
-                'real data',
-                'The synthetic data follows over 90% of the min/max boundaries set by the '
-                'real data',
-                'Over 90% of the synthetic rows are not copies of the real data'
-            ],
-            'WARNING': [],
-            'DANGER': []
-        }
-
-        assert result_dict == expected_results
+        assert result_dict == 0.975666343427581
 
     def test_end_to_end(self):
         """Test the end-to-end functionality of the diagnostic report."""
@@ -88,59 +63,40 @@ class TestDiagnosticReport:
         report.generate(real_data, synthetic_data, metadata)
 
         # Assert
-        expected_details_synthetis = pd.DataFrame(
-            {
-                'Metric': 'NewRowSynthesis',
-                'Score': 1.0,
-                'Num Matched Rows': 0,
-                'Num New Rows': 215
-            }, index=[0]
-        )
-
-        expected_details_coverage = pd.DataFrame({
+        expected_details_data_validity = pd.DataFrame({
             'Column': [
-                'start_date', 'end_date', 'salary', 'duration', 'high_perc', 'high_spec',
-                'mba_spec', 'second_perc', 'gender', 'degree_perc', 'placed', 'experience_years',
-                'employability_perc', 'mba_perc', 'work_experience', 'degree_type'
+                'start_date', 'end_date', 'salary', 'duration', 'student_id', 'high_perc',
+                'high_spec', 'mba_spec', 'second_perc', 'gender', 'degree_perc', 'placed',
+                'experience_years', 'employability_perc', 'mba_perc', 'work_experience',
+                'degree_type'
             ],
             'Metric': [
-                'RangeCoverage', 'RangeCoverage', 'RangeCoverage', 'RangeCoverage',
-                'RangeCoverage', 'CategoryCoverage', 'CategoryCoverage', 'RangeCoverage',
-                'CategoryCoverage', 'RangeCoverage', 'CategoryCoverage', 'RangeCoverage',
-                'RangeCoverage', 'RangeCoverage', 'CategoryCoverage', 'CategoryCoverage'
+                'BoundaryAdherence', 'BoundaryAdherence', 'BoundaryAdherence', 'BoundaryAdherence',
+                'KeyUniqueness', 'BoundaryAdherence', 'CategoryAdherence', 'CategoryAdherence',
+                'BoundaryAdherence', 'CategoryAdherence', 'BoundaryAdherence', 'CategoryAdherence',
+                'BoundaryAdherence', 'BoundaryAdherence', 'BoundaryAdherence', 'CategoryAdherence',
+                'CategoryAdherence'
             ],
             'Score': [
-                1.0, 1.0, 0.42333783783783785, 1.0, 0.9807348482826732, 1.0, 1.0, 1.0, 1.0, 1.0,
-                1.0, 0.6666666666666667, 1.0, 1.0, 1.0, 1.0
+                0.8503937007874016, 0.8615384615384616, 0.9444444444444444, 1.0, 1.0,
+                0.8651162790697674, 1.0, 1.0, 0.9255813953488372, 1.0, 0.9441860465116279, 1.0,
+                1.0, 0.8883720930232558, 0.8930232558139535, 1.0, 1.0
             ]
         })
 
-        expected_details_boundary = pd.DataFrame({
-            'Column': [
-                'start_date', 'end_date', 'salary', 'duration', 'high_perc', 'second_perc',
-                'degree_perc', 'experience_years', 'employability_perc', 'mba_perc'
-            ],
-            'Metric': ['BoundaryAdherence'] * 10,
-            'Score': [
-                0.8503937007874016, 0.8615384615384616, 0.9444444444444444, 1.0,
-                0.8651162790697674, 0.9255813953488372, 0.9441860465116279, 1.0,
-                0.8883720930232558, 0.8930232558139535
-            ]
+        expected_details_data_structure = pd.DataFrame({
+            'Metric': ['TableFormat'],
+            'Score': [1.0]
         })
 
         pd.testing.assert_frame_equal(
-            report.get_details('Synthesis'),
-            expected_details_synthetis
+            report.get_details('Data Validity'),
+            expected_details_data_validity
         )
 
         pd.testing.assert_frame_equal(
-            report.get_details('Coverage'),
-            expected_details_coverage
-        )
-
-        pd.testing.assert_frame_equal(
-            report.get_details('Boundary'),
-            expected_details_boundary
+            report.get_details('Data Structure'),
+            expected_details_data_structure
         )
 
     def test_generate_with_object_datetimes(self):
@@ -158,59 +114,40 @@ class TestDiagnosticReport:
         report.generate(real_data, synthetic_data, metadata)
 
         # Assert
-        expected_details_synthetis = pd.DataFrame(
-            {
-                'Metric': 'NewRowSynthesis',
-                'Score': 1.0,
-                'Num Matched Rows': 0,
-                'Num New Rows': 215
-            }, index=[0]
-        )
-
-        expected_details_coverage = pd.DataFrame({
+        expected_details_data_validity = pd.DataFrame({
             'Column': [
-                'start_date', 'end_date', 'salary', 'duration', 'high_perc', 'high_spec',
-                'mba_spec', 'second_perc', 'gender', 'degree_perc', 'placed', 'experience_years',
-                'employability_perc', 'mba_perc', 'work_experience', 'degree_type'
+                'start_date', 'end_date', 'salary', 'duration', 'student_id', 'high_perc',
+                'high_spec', 'mba_spec', 'second_perc', 'gender', 'degree_perc', 'placed',
+                'experience_years', 'employability_perc', 'mba_perc', 'work_experience',
+                'degree_type'
             ],
             'Metric': [
-                'RangeCoverage', 'RangeCoverage', 'RangeCoverage', 'RangeCoverage',
-                'RangeCoverage', 'CategoryCoverage', 'CategoryCoverage', 'RangeCoverage',
-                'CategoryCoverage', 'RangeCoverage', 'CategoryCoverage', 'RangeCoverage',
-                'RangeCoverage', 'RangeCoverage', 'CategoryCoverage', 'CategoryCoverage'
+                'BoundaryAdherence', 'BoundaryAdherence', 'BoundaryAdherence', 'BoundaryAdherence',
+                'KeyUniqueness', 'BoundaryAdherence', 'CategoryAdherence', 'CategoryAdherence',
+                'BoundaryAdherence', 'CategoryAdherence', 'BoundaryAdherence', 'CategoryAdherence',
+                'BoundaryAdherence', 'BoundaryAdherence', 'BoundaryAdherence', 'CategoryAdherence',
+                'CategoryAdherence'
             ],
             'Score': [
-                1.0, 1.0, 0.42333783783783785, 1.0, 0.9807348482826732, 1.0, 1.0, 1.0, 1.0, 1.0,
-                1.0, 0.6666666666666667, 1.0, 1.0, 1.0, 1.0
+                0.8503937007874016, 0.8615384615384616, 0.9444444444444444, 1.0, 1.0,
+                0.8651162790697674, 1.0, 1.0, 0.9255813953488372, 1.0, 0.9441860465116279,
+                1.0, 1.0, 0.8883720930232558, 0.8930232558139535, 1.0, 1.0
             ]
         })
 
-        expected_details_boundary = pd.DataFrame({
-            'Column': [
-                'start_date', 'end_date', 'salary', 'duration', 'high_perc', 'second_perc',
-                'degree_perc', 'experience_years', 'employability_perc', 'mba_perc'
-            ],
-            'Metric': ['BoundaryAdherence'] * 10,
-            'Score': [
-                0.8503937007874016, 0.8615384615384616, 0.9444444444444444, 1.0,
-                0.8651162790697674, 0.9255813953488372, 0.9441860465116279, 1.0,
-                0.8883720930232558, 0.8930232558139535
-            ]
+        expected_details_data_structure = pd.DataFrame({
+            'Metric': ['TableFormat'],
+            'Score': [1.0]
         })
 
         pd.testing.assert_frame_equal(
-            report.get_details('Synthesis'),
-            expected_details_synthetis
+            report.get_details('Data Validity'),
+            expected_details_data_validity
         )
 
         pd.testing.assert_frame_equal(
-            report.get_details('Coverage'),
-            expected_details_coverage
-        )
-
-        pd.testing.assert_frame_equal(
-            report.get_details('Boundary'),
-            expected_details_boundary
+            report.get_details('Data Structure'),
+            expected_details_data_structure
         )
 
     def test_generate_multiple_times(self):
@@ -222,84 +159,48 @@ class TestDiagnosticReport:
         # Run and assert
         report = DiagnosticReport()
         report.generate(real_data, synthetic_data, metadata, verbose=False)
-        expected_results = {
-            'DANGER': [],
-            'SUCCESS': [
-                'The synthetic data covers over 90% of the categories present in the real data',
-                'The synthetic data covers over 90% of the numerical ranges present in the real '
-                'data',
-                'The synthetic data follows over 90% of the min/max boundaries set by the real '
-                'data',
-                'Over 90% of the synthetic rows are not copies of the real data'
-            ],
-            'WARNING': []
-        }
-        assert report.get_results() == expected_results
+
+        assert report.get_score() == 0.975666343427581
         report.generate(real_data, synthetic_data, metadata)
-        assert report.get_results() == expected_results
+        assert report.get_score() == 0.975666343427581
 
     def test_get_details_with_errors(self):
         """Test the ``get_details`` function of the diagnostic report when there are errors."""
         # Setup
         real_data, synthetic_data, metadata = load_demo(modality='single_table')
         report = DiagnosticReport()
-        real_data['second_perc'] = np.nan
+        real_data['second_perc'] = 'A'
 
         # Run
         report.generate(real_data, synthetic_data, metadata)
 
         # Assert
         expected_details = pd.DataFrame({
-            'Column': {
-                0: 'start_date',
-                1: 'end_date',
-                2: 'salary',
-                3: 'duration',
-                4: 'high_perc',
-                5: 'second_perc',
-                6: 'degree_perc',
-                7: 'experience_years',
-                8: 'employability_perc',
-                9: 'mba_perc'
-            },
-            'Metric': {
-                0: 'BoundaryAdherence',
-                1: 'BoundaryAdherence',
-                2: 'BoundaryAdherence',
-                3: 'BoundaryAdherence',
-                4: 'BoundaryAdherence',
-                5: 'BoundaryAdherence',
-                6: 'BoundaryAdherence',
-                7: 'BoundaryAdherence',
-                8: 'BoundaryAdherence',
-                9: 'BoundaryAdherence'
-            },
-            'Score': {
-                0: 0.8503937007874016,
-                1: 0.8615384615384616,
-                2: 0.9444444444444444,
-                3: 1.0,
-                4: 0.8651162790697674,
-                5: np.nan,
-                6: 0.9441860465116279,
-                7: 1.0,
-                8: 0.8883720930232558,
-                9: 0.8930232558139535
-            },
-            'Error': {
-                0: None,
-                1: None,
-                2: None,
-                3: None,
-                4: None,
-                5: 'InvalidDataError: All NaN values in real data.',
-                6: None,
-                7: None,
-                8: None,
-                9: None
-            }
+            'Column': [
+                'start_date', 'end_date', 'salary', 'duration', 'student_id', 'high_perc',
+                'high_spec', 'mba_spec', 'second_perc', 'gender', 'degree_perc', 'placed',
+                'experience_years', 'employability_perc', 'mba_perc', 'work_experience',
+                'degree_type'
+            ],
+            'Metric': [
+                'BoundaryAdherence', 'BoundaryAdherence', 'BoundaryAdherence', 'BoundaryAdherence',
+                'KeyUniqueness', 'BoundaryAdherence', 'CategoryAdherence', 'CategoryAdherence',
+                'BoundaryAdherence', 'CategoryAdherence', 'BoundaryAdherence', 'CategoryAdherence',
+                'BoundaryAdherence', 'BoundaryAdherence', 'BoundaryAdherence', 'CategoryAdherence',
+                'CategoryAdherence'
+            ],
+            'Score': [
+                0.8503937007874016, 0.8615384615384616, 0.9444444444444444, 1.0, 1.0,
+                0.8651162790697674, 1.0, 1.0, np.nan, 1.0, 0.9441860465116279, 1.0, 1.0,
+                0.8883720930232558, 0.8930232558139535, 1.0, 1.0
+            ],
+            'Error': [
+                None, None, None, None, None, None, None, None,
+                'TypeError: Invalid comparison between dtype=float64 and str',
+                None, None, None, None, None, None, None, None
+            ]
         })
         pd.testing.assert_frame_equal(
-            report.get_details('Boundary'),
+            report.get_details('Data Validity'),
             expected_details
         )
