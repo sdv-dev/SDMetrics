396c9735e159edad1276bcac3408eac0e8815400
R-Palazzo <116157184+R-Palazzo@users.noreply.github.com>
2023-11-13 14:48:40 -0600

    KeyUniqueness metric should only be applied to primary and alternate keys (#511)

--- tests/integration/reports/multi_table/_properties/test_data_validity.py
+++ tests/integration/reports/multi_table/_properties/test_data_validity.py
@@ -18,7 +18,7 @@ class TestDataValidity:
         result = column_shapes.get_score(real_data, synthetic_data, metadata)
 
         # Assert
-        assert result == 0.9444444444444445
+        assert result == 0.9636363636363637
 
     def test_with_progress_bar(self):
         """Test that the progress bar is correctly updated."""
@@ -35,5 +35,5 @@ class TestDataValidity:
         result = column_shapes.get_score(real_data, synthetic_data, metadata, progress_bar)
 
         # Assert
-        assert result == 0.9444444444444445
+        assert result == 0.9636363636363637
         assert mock_update.call_count == num_columns
