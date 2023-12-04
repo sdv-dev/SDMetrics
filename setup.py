251fe444067679da7aa235ce3bf14d118c552c72
Andrew Montanez <andrew@sdv.dev>
2023-12-04 13:06:13 -0600

    Bump version: 0.13.0 → 0.13.1.dev0

--- setup.py
+++ setup.py
@@ -128,6 +128,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/SDMetrics',
-    version='0.13.0',
+    version='0.13.1.dev0',
     zip_safe=False,
 )
