932298fb08b1a3bd8950ec9cc0a0432b1d0a86b8
lajohn4747 <john@datacebo.com>
2024-03-25 11:05:42 -0500

    Remove percentage from bar

--- sdmetrics/reports/base_report.py
+++ sdmetrics/reports/base_report.py
@@ -172,9 +172,9 @@ class BaseReport():
         for ind, (property_name, property_instance) in enumerate(self._properties.items()):
             if verbose:
                 num_iterations = int(property_instance._get_num_iterations(metadata))
-                progress_bar = tqdm.tqdm(total=num_iterations, file=sys.stdout)
+                progress_bar = tqdm.tqdm(total=num_iterations, file=sys.stdout, bar_format='{desc}|{bar}{r_bar}|')
                 progress_bar.set_description(
-                    f'({ind + 1}/{len(self._properties)}) Evaluating {property_name}: '
+                    f'({ind + 1}/{len(self._properties)}) Evaluating {property_name}'
                 )
 
             score = self._properties[property_name].get_score(
