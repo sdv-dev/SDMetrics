4f8670f8662a8e0235f52e1fe92ad668d5c56987
Felipe Alex Hofmann <fealho@gmail.com>
2024-04-18 09:54:04 -0700

    Fix `entry_points` warning message (#564)

--- sdmetrics/__init__.py
+++ sdmetrics/__init__.py
@@ -144,7 +144,7 @@ def _find_addons():
         try:
             addon = entry_point.load()
         except Exception:  # pylint: disable=broad-exception-caught
-            msg = f'Failed to load "{entry_point.name}" from "{entry_point.module_name}".'
+            msg = f'Failed to load "{entry_point.name}" from "{entry_point.value}".'
             python_warnings.warn(msg)
             continue
 
