f2240f8cf31059e9c634090fd1e8589d4e683732
Andrew Montanez <andrew@sdv.dev>
2024-08-27 11:14:35 -0500

    picking simplest and fastest solution and fixing rounding errors for tests

--- tests/integration/reports/multi_table/_properties/test_inter_table_trends.py
+++ tests/integration/reports/multi_table/_properties/test_inter_table_trends.py
@@ -17,7 +17,7 @@ class TestInterTableTrends:
         result = inter_table_trends.get_score(real_data, synthetic_data, metadata)
 
         # Assert
-        assert result == 0.4416666666666667
+        assert result == 0.4416666666666666
 
     def test_with_progress_bar(self):
         """Test that the progress bar is correctly updated."""
@@ -38,5 +38,5 @@ class TestInterTableTrends:
         result = inter_table_trends.get_score(real_data, synthetic_data, metadata, progress_bar)
 
         # Assert
-        assert result == 0.4416666666666667
+        assert result == 0.4416666666666666
         assert mock_update.call_count == num_iter
