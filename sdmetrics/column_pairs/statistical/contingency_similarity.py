be778840343b01f508923cd4e5418ac4d8c6c18a
Andrew Montanez <andrew@sdv.dev>
2024-08-29 12:07:59 -0500

    Try to improve performance of contingency_similarity (#625)

--- sdmetrics/column_pairs/statistical/contingency_similarity.py
+++ sdmetrics/column_pairs/statistical/contingency_similarity.py
@@ -1,7 +1,5 @@
 """Contingency Similarity Metric."""
 
-import pandas as pd
-
 from sdmetrics.column_pairs.base import ColumnPairsMetric
 from sdmetrics.goal import Goal
 
@@ -42,43 +40,16 @@ class ContingencySimilarity(ColumnPairsMetric):
         columns = real_data.columns[:2]
         real = real_data[columns]
         synthetic = synthetic_data[columns]
-        contingency_real = pd.crosstab(
-            index=real[columns[0]].astype(str),
-            columns=real[columns[1]].astype(str),
-            normalize=True,
+        contingency_real = real.groupby(list(columns), dropna=False).size() / len(real)
+        contingency_synthetic = synthetic.groupby(list(columns), dropna=False).size() / len(
+            synthetic
         )
-        contingency_synthetic = pd.crosstab(
-            index=synthetic[columns[0]].astype(str),
-            columns=synthetic[columns[1]].astype(str),
-            normalize=True,
-        )
-
-        real_append_cols = {}
-        for col in set(contingency_synthetic.columns) - set(contingency_real.columns):
-            real_append_cols[col] = [0 for _ in range(len(contingency_real))]
-        contingency_real = pd.concat(
-            (contingency_real, pd.DataFrame(real_append_cols, index=contingency_real.index)),
-            axis=1,
-        )
-
-        synthetic_append_cols = {}
-        for col in set(contingency_real.columns) - set(contingency_synthetic.columns):
-            synthetic_append_cols[col] = [0 for _ in range(len(contingency_synthetic))]
-        contingency_synthetic = pd.concat(
-            (
-                contingency_synthetic,
-                pd.DataFrame(synthetic_append_cols, index=contingency_synthetic.index),
-            ),
-            axis=1,
-        )
-
-        for row in set(contingency_synthetic.index) - set(contingency_real.index):
-            contingency_real.loc[row] = [0 for _ in range(len(contingency_real.columns))]
-        for row in set(contingency_real.index) - set(contingency_synthetic.index):
-            contingency_synthetic.loc[row] = [0 for _ in range(len(contingency_synthetic.columns))]
-
-        variation = abs(contingency_real - contingency_synthetic) / 2
-        return 1 - variation.sum().sum()
+        combined_index = contingency_real.index.union(contingency_synthetic.index)
+        contingency_synthetic = contingency_synthetic.reindex(combined_index, fill_value=0)
+        contingency_real = contingency_real.reindex(combined_index, fill_value=0)
+        diff = abs(contingency_real - contingency_synthetic).fillna(0)
+        variation = diff / 2
+        return 1 - variation.sum()
 
     @classmethod
     def normalize(cls, raw_score):
