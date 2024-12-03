253724e4beb004fb758ad28218879cfa1e182805
Felipe Alex Hofmann <fealho@gmail.com>
2024-12-03 08:45:08 -0800

    Fix missing values handling for `InterRowMSAS`  (#680)

--- tests/unit/column_pairs/statistical/test_inter_row_msas.py
+++ tests/unit/column_pairs/statistical/test_inter_row_msas.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+import numpy as np
 import pandas as pd
 import pytest
 
@@ -7,6 +8,24 @@ from sdmetrics.column_pairs import InterRowMSAS
 
 
 class TestInterRowMSAS:
+    def test_compute_breakdown(self):
+        """Test `compute_breakdown` works."""
+        # Setup
+        real_keys = pd.Series(['id1', 'id1', 'id1', 'id2', 'id2', 'id2'])
+        real_values = pd.Series([1, 2, 3, 4, 5, 6])
+        synthetic_keys = pd.Series(['id3', 'id3', 'id3', 'id4', 'id4', 'id4'])
+        synthetic_values = pd.Series([1, 10, 3, 7, 5, 1])
+
+        metric = InterRowMSAS()
+
+        # Run
+        result = metric.compute_breakdown(
+            real_data=(real_keys, real_values), synthetic_data=(synthetic_keys, synthetic_values)
+        )
+
+        # Assert
+        assert result == {'score': 0.5}
+
     def test_compute(self):
         """Test it runs."""
         # Setup
@@ -14,6 +33,22 @@ class TestInterRowMSAS:
         real_values = pd.Series([1, 2, 3, 4, 5, 6])
         synthetic_keys = pd.Series(['id3', 'id3', 'id3', 'id4', 'id4', 'id4'])
         synthetic_values = pd.Series([1, 10, 3, 7, 5, 1])
+
+        # Run
+        score = InterRowMSAS.compute(
+            real_data=(real_keys, real_values), synthetic_data=(synthetic_keys, synthetic_values)
+        )
+
+        # Assert
+        assert score == 0.5
+
+    def test_compute_nans(self):
+        """Test it runs with nans."""
+        # Setup
+        real_keys = pd.Series(['id1', 'id1', 'id1', 'id2', 'id2', 'id2'])
+        real_values = pd.Series([1, 2, np.nan, 4, 5, 8])
+        synthetic_keys = pd.Series(['id3', 'id3', 'id3', 'id4', 'id4', 'id4'])
+        synthetic_values = pd.Series([1, 10, 4, 7, np.nan, np.nan])
 
         # Run
         score = InterRowMSAS.compute(
@@ -94,9 +129,10 @@ class TestInterRowMSAS:
             'There are 3 non-positive values in your data, which cannot be used with log. '
             "Consider changing 'apply_log' to False for a better result."
         )
+
         assert len(warning_info) == 1
         assert str(warning_info[0].message) == expected_message
-        assert score == 0
+        assert score == 0.5
 
     def test_compute_with_log_datetime(self):
         """Test it crashes for logs of datetime values."""
@@ -211,7 +247,9 @@ class TestInterRowMSAS:
         synthetic_values = pd.Series([1, 10, 3, 7, 5, 1])
 
         # Run and Assert
-        warn_msg = "n_rows_diff '10' is greater than the size of 2 sequence keys in real_data."
+        warn_msg = (
+            "n_rows_diff '10' is greater or equal to the size of 2 sequence keys in real_data."
+        )
         with pytest.warns(UserWarning, match=warn_msg):
             score = InterRowMSAS.compute(
                 real_data=(real_keys, real_values),
