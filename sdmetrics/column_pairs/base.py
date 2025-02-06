cf243430f5f45fa97f3ec81e3727e8cac68f0ff8
R-Palazzo <romainpalazzo@gmail.com>
2025-02-06 17:26:29 +0000

    setup investigation

--- sdmetrics/column_pairs/base.py
+++ sdmetrics/column_pairs/base.py
@@ -1,7 +1,10 @@
 """Base class for metrics that compare pairs of columns."""
 
 from sdmetrics.base import BaseMetric
-
+from time import process_time
+import numpy as np
+DEFAULT_NUM_ROWS = None
+DEFAULT_NUM_TRY = None
 
 class ColumnPairsMetric(BaseMetric):
     """Base class for metrics that compare pairs of columns.
@@ -42,18 +45,19 @@ class ColumnPairsMetric(BaseMetric):
 
     @classmethod
     def compute_breakdown(cls, real_data, synthetic_data):
-        """Compute the breakdown of this metric.
+        """Compute the breakdown of this metric."""
+        start = process_time()
+        num_try = 1 if DEFAULT_NUM_TRY is None else DEFAULT_NUM_TRY
+        result = np.zeros(num_try)
+        for i in range(num_try):
+            if DEFAULT_NUM_ROWS is not None:
+                real_to_subsample = min(DEFAULT_NUM_ROWS, len(real_data))
+                real_data_to_compute = real_data.sample(n=real_to_subsample)
+                synthetic_data_to_compute = synthetic_data.sample(n=real_to_subsample)
 
-        Args:
-            real_data (pandas.DataFrame):
-                The values from the real dataset, passed as pandas.DataFrame
-                with 2 columns.
-            synthetic_data (pandas.DataFrame):
-                The values from the synthetic dataset, passed as a
-                pandas.DataFrame with 2 columns.
+            result[i] = cls.compute(real_data_to_compute, synthetic_data_to_compute)
 
-        Returns:
-            dict
-                A mapping of the metric output. Must contain the key 'score'.
-        """
-        return {'score': cls.compute(real_data, synthetic_data)}
+        score = np.mean(result)
+        end = process_time()
+
+        return {'score': score, 'time': end - start, 'num_rows': DEFAULT_NUM_ROWS}
