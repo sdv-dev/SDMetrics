1d6c98f30627441e012f7a2562a901d967cae844
R-Palazzo <romainpalazzo@gmail.com>
2025-02-11 15:18:42 +0000

    def

--- sdmetrics/column_pairs/statistical/contingency_similarity.py
+++ sdmetrics/column_pairs/statistical/contingency_similarity.py
@@ -27,7 +27,9 @@ class ContingencySimilarity(ColumnPairsMetric):
     max_value = 1.0
 
     @staticmethod
-    def _validate_inputs(real_data, synthetic_data, continuous_column_names, num_discrete_bins):
+    def _validate_inputs(
+        real_data, synthetic_data, continuous_column_names, num_discrete_bins, num_rows_subsample
+    ):
         for data in [real_data, synthetic_data]:
             if not isinstance(data, pd.DataFrame) or len(data.columns) != 2:
                 raise ValueError('The data must be a pandas DataFrame with two columns.')
@@ -47,8 +49,19 @@ class ContingencySimilarity(ColumnPairsMetric):
         if not isinstance(num_discrete_bins, int) or num_discrete_bins <= 0:
             raise ValueError('`num_discrete_bins` must be an integer greater than zero.')
 
+        if num_rows_subsample is not None:
+            if not isinstance(num_rows_subsample, int) or num_rows_subsample <= 0:
+                raise ValueError('`num_rows_subsample` must be an integer greater than zero.')
+
     @classmethod
-    def compute(cls, real_data, synthetic_data, continuous_column_names=None, num_discrete_bins=10):
+    def compute(
+        cls,
+        real_data,
+        synthetic_data,
+        continuous_column_names=None,
+        num_discrete_bins=10,
+        num_rows_subsample=None,
+    ):
         """Compare the contingency similarity of two discrete columns.
 
         Args:
@@ -62,13 +75,27 @@ class ContingencySimilarity(ColumnPairsMetric):
                 to ``None``.
             num_discrete_bins (int, optional):
                 The number of bins to create for the continuous columns. Defaults to 10.
+            num_rows_subsample (int, optional):
+                The number of rows to subsample from the real and synthetic data before computing
+                the metric. Defaults to ``None``.
 
         Returns:
             float:
                 The contingency similarity of the two columns.
         """
-        cls._validate_inputs(real_data, synthetic_data, continuous_column_names, num_discrete_bins)
+        cls._validate_inputs(
+            real_data,
+            synthetic_data,
+            continuous_column_names,
+            num_discrete_bins,
+            num_rows_subsample,
+        )
         columns = real_data.columns[:2]
+
+        if num_rows_subsample is not None:
+            real_data = real_data.sample(min(num_rows_subsample, len(real_data)))
+            synthetic_data = synthetic_data.sample(min(num_rows_subsample, len(synthetic_data)))
+
         real = real_data[columns]
         synthetic = synthetic_data[columns]
         if continuous_column_names is not None:
