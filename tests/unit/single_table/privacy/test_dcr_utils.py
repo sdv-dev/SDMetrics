e21085ff17e419f482c4c9e8230a8431f527d650
lajohn4747 <john@datacebo.com>
2025-02-19 16:29:31 -0600

    Use apply instead of iterating through the dataframe

--- tests/unit/single_table/privacy/test_dcr_utils.py
+++ tests/unit/single_table/privacy/test_dcr_utils.py
@@ -12,6 +12,7 @@ from sdmetrics.single_table.privacy.dcr_utils import (
     _calculate_dcr_value,
     calculate_dcr,
 )
+from tests.utils import check_if_value_in_threshold
 
 
 @pytest.fixture()
@@ -156,10 +157,6 @@ SECONDS_IN_DAY = 86400
 ACCURACY_THRESHOLD = 0.000001
 
 
-def check_if_value_in_threshold(value, expected_value, threshold=ACCURACY_THRESHOLD):
-    assert abs(value - expected_value) < threshold
-
-
 @pytest.mark.parametrize(
     's_value, d_value, range, col_name, expected_dist',
     [
@@ -242,7 +239,7 @@ def test__calculate_dcr_between_rows(
     # Assert
     for i in range(len(expected_row_comparisons)):
         expected_dist = sum(expected_row_comparisons[i]) / len(expected_row_comparisons[i])
-        check_if_value_in_threshold(result[i], expected_dist)
+        check_if_value_in_threshold(result[i], expected_dist, ACCURACY_THRESHOLD)
 
 
 def test__calculate_dcr_between_rows_bad_index():
@@ -258,7 +255,7 @@ def test__calculate_dcr_between_rows_bad_index():
     })
     metadata = {'columns': {'A': {'sdtype': 'numerical'}, 'B': {'sdtype': 'numerical'}}}
     test_range = {'A': 1.0, 'C': 1.0}
-    error_msg = re.escape('Column name (B) was not found when calculating DCR between two rows.')
+    error_msg = re.escape("Missing columns in comparison_row: {'B'}")
 
     # Assert
     with pytest.raises(ValueError, match=error_msg):
@@ -283,7 +280,7 @@ def test__calculate_dcr_between_row_and_data(
 
     # Assert
     for i in range(len(expected_dcr_result)):
-        check_if_value_in_threshold(result[i], expected_dcr_result[i])
+        check_if_value_in_threshold(result[i], expected_dcr_result[i], ACCURACY_THRESHOLD)
 
 
 def test_calculate_dcr(
