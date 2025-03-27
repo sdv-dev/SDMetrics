a75fdd08c5f9476d62fef72b3382900f02fea88b
John La <lajohn4747@users.noreply.github.com>
2025-03-27 09:11:39 -0500

    Optimize DCR calculation using vector operations (#754)

--- sdmetrics/single_table/privacy/dcr_utils.py
+++ sdmetrics/single_table/privacy/dcr_utils.py
@@ -1,156 +1,116 @@
 """Distance to closest record measurement functions."""
 
+import numpy as np
 import pandas as pd
 
 from sdmetrics._utils_metadata import _process_data_with_metadata
 from sdmetrics.utils import get_columns_from_metadata
 
+CHUNK_SIZE = 1000
 
-def _calculate_dcr_value(synthetic_value, real_value, sdtype, col_range=None):
-    """Calculate the Distance to Closest Record between two different values.
 
-    Arguments:
-        synthetic_value (int, float, datetime, boolean, string, or None):
-            The synthetic value that we are calculating DCR value for
-        real_value (int, float, datetime, boolean, string, or None):
-            The data value that we are referencing for measuring DCR.
-        sdtype (string):
-            The sdtype of the column values.
-        col_range (float):
-            The range of values for a column used for numerical values to calculate DCR.
-            Defaults to None.
+def _process_dcr_chunk(chunk, reference_copy, cols_to_keep, metadata, ranges):
+    full_dataset = chunk.merge(reference_copy, how='cross', suffixes=('_data', '_ref'))
 
-    Returns:
-       float:
-            Returns dcr value between two given values.
-    """
-    if pd.isna(synthetic_value) and pd.isna(real_value):
-        return 0.0
-    elif pd.isna(synthetic_value) or pd.isna(real_value):
-        return 1.0
+    for col_name in cols_to_keep:
+        sdtype = metadata['columns'][col_name]['sdtype']
+        ref_column = full_dataset[col_name + '_ref']
+        data_column = full_dataset[col_name + '_data']
+        diff_col_name = col_name + '_diff'
+        if sdtype in ['numerical', 'datetime']:
+            diff = (ref_column - data_column).abs()
+            if pd.api.types.is_timedelta64_dtype(diff):
+                diff = diff.dt.total_seconds()
 
-    if sdtype == 'numerical' or sdtype == 'datetime':
-        if col_range is None:
-            raise ValueError(
-                'No col_range was provided. The col_range is required '
-                'for numerical and datetime sdtype DCR calculation.'
+            full_dataset[col_name + '_diff'] = np.where(
+                ranges[col_name] == 0,
+                (diff > 0).astype(int),
+                np.minimum(diff / ranges[col_name], 1.0),
             )
 
-        difference = abs(synthetic_value - real_value)
-        if isinstance(difference, pd.Timedelta):
-            difference = difference.total_seconds()
+            xor_condition = (ref_column.isna() & ~data_column.isna()) | (
+                ~ref_column.isna() & data_column.isna()
+            )
 
-        distance = 0.0 if synthetic_value == real_value else 1.0
-        if col_range != 0:
-            distance = difference / col_range
+            full_dataset.loc[xor_condition, diff_col_name] = 1
 
-        return min(distance, 1.0)
+            both_nan_condition = ref_column.isna() & data_column.isna()
 
-    if synthetic_value == real_value:
-        return 0.0
-    else:
-        return 1.0
+            full_dataset.loc[both_nan_condition, diff_col_name] = 0
 
+        elif sdtype in ['categorical', 'boolean']:
+            equals_cat = (ref_column == data_column) | (ref_column.isna() & data_column.isna())
+            full_dataset[diff_col_name] = (~equals_cat).astype(int)
 
-def _calculate_dcr_between_rows(synthetic_row, comparison_row, column_ranges, metadata):
-    """Calculate the Distance to Closest Record between two rows.
+        full_dataset.drop(columns=[col_name + '_ref', col_name + '_data'], inplace=True)
 
-    Arguments:
-        synthetic_row (pandas.Series):
-            The synthetic row that we are calculating DCR value for.
-        comparison_row (pandas.Series):
-            The data value that we are referencing for measuring DCR.
-        column_ranges (dict):
-            A dictionary that defines the range for each numerical column.
-        metadata (dict):
-            The metadata dict.
-
-    Returns:
-        float:
-            Returns DCR value (the average value of DCR values we computed across the row).
-    """
-    dcr_values = synthetic_row.index.to_series().apply(
-        lambda synthetic_column_name: _calculate_dcr_value(
-            synthetic_row[synthetic_column_name],
-            comparison_row[synthetic_column_name],
-            metadata['columns'][synthetic_column_name]['sdtype'],
-            column_ranges.get(synthetic_column_name),
-        )
+    full_dataset['diff'] = full_dataset.iloc[:, 2:].sum(axis=1) / len(cols_to_keep)
+    chunk_result = (
+        full_dataset[['index_data', 'diff']].groupby('index_data').min().reset_index(drop=True)
     )
-
-    return dcr_values.mean()
-
-
-def _calculate_dcr_between_row_and_data(synthetic_row, real_data, column_ranges, metadata):
-    """Calculate the DCR between a single row in the synthetic data and another dataset.
-
-    Arguments:
-        synthetic_row (pandas.Series):
-            The synthetic row that we are calculating DCR against an entire dataset.
-        real_data (pandas.Dataframe):
-            The dataset that acts as the reference for DCR calculations.
-        column_ranges (dict):
-            A dictionary that defines the range for each numerical column.
-        metadata (dict):
-            The metadata dict.
-
-    Returns:
-        float:
-            Returns the minimum distance to closest record computed between the
-            synthetic row and the reference dataset.
-    """
-    synthetic_distance_to_all_real = real_data.apply(
-        lambda real_row: _calculate_dcr_between_rows(
-            synthetic_row, real_row, column_ranges, metadata
-        ),
-        axis=1,
-    )
-    return synthetic_distance_to_all_real.min()
+    return chunk_result['diff']
 
 
-def calculate_dcr(real_data, synthetic_data, metadata):
+def calculate_dcr(dataset, reference_dataset, metadata):
     """Calculate the Distance to Closest Record for all rows in the synthetic data.
 
     Arguments:
-        real_data (pandas.Dataframe):
-            The dataset that acts as the reference for DCR calculations. Ranges are determined from
-            this dataset.
-        synthetic_data (pandas.Dataframe):
-            The synthetic data that we are calculating DCR values for. Every row will be measured
-            against the comparison data.
+        dataset (pandas.Dataframe):
+            The dataset for which we want to compute the DCR values
+        reference_dataset (pandas.Dataframe):
+            The reference dataset that is used for the distance computations
         metadata (dict):
             The metadata dict.
 
     Returns:
         pandas.Series:
-            Returns a Series that shows the DCR value for every row of synthetic data.
+            Returns a Series that shows the DCR value for every row of dataset
     """
-    column_ranges = {}
+    dataset_copy = _process_data_with_metadata(dataset.copy(), metadata, True)
+    reference_copy = _process_data_with_metadata(reference_dataset.copy(), metadata, True)
 
-    real_data_copy = real_data.copy()
-    synthetic_data_copy = synthetic_data.copy()
-    real_data_copy = _process_data_with_metadata(real_data_copy, metadata, True)
-    synthetic_data_copy = _process_data_with_metadata(synthetic_data_copy, metadata, True)
+    common_cols = set(dataset_copy.columns) & set(reference_copy.columns)
+    cols_to_keep = []
+    ranges = {}
 
-    overlapping_columns = set(real_data_copy.columns) & set(synthetic_data_copy.columns)
-    if not overlapping_columns:
+    for col_name, col_metadata in get_columns_from_metadata(metadata).items():
+        sdtype = col_metadata['sdtype']
+
+        if (
+            sdtype in ['numerical', 'categorical', 'boolean', 'datetime']
+            and col_name in common_cols
+        ):
+            cols_to_keep.append(col_name)
+
+            if sdtype in ['numerical', 'datetime']:
+                col_range = reference_copy[col_name].max() - reference_copy[col_name].min()
+                if isinstance(col_range, pd.Timedelta):
+                    col_range = col_range.total_seconds()
+
+                ranges[col_name] = col_range
+
+    if not cols_to_keep:
         raise ValueError('There are no overlapping statistical columns to measure.')
 
-    for col_name, column in get_columns_from_metadata(metadata).items():
-        sdtype = column['sdtype']
-        col_range = None
-        if sdtype == 'numerical' or sdtype == 'datetime':
-            col_range = real_data_copy[col_name].max() - real_data_copy[col_name].min()
-            if isinstance(col_range, pd.Timedelta):
-                col_range = col_range.total_seconds()
+    dataset_copy = dataset_copy[cols_to_keep]
+    dataset_copy['index'] = range(len(dataset_copy))
 
-        column_ranges[col_name] = col_range
+    reference_copy = reference_copy[cols_to_keep]
+    reference_copy['index'] = range(len(reference_copy))
+    results = []
 
-    dcr_dist_df = synthetic_data_copy.apply(
-        lambda synth_row: _calculate_dcr_between_row_and_data(
-            synth_row, real_data_copy, column_ranges, metadata
-        ),
-        axis=1,
-    )
+    for chunk_start in range(0, len(dataset_copy), CHUNK_SIZE):
+        chunk = dataset_copy.iloc[chunk_start : chunk_start + CHUNK_SIZE].copy()
+        chunk_result = _process_dcr_chunk(
+            chunk=chunk,
+            reference_copy=reference_copy,
+            cols_to_keep=cols_to_keep,
+            metadata=metadata,
+            ranges=ranges,
+        )
+        results.append(chunk_result)
 
-    return dcr_dist_df
+    result = pd.concat(results, ignore_index=True)
+    result.name = None
+
+    return result
--- tests/unit/single_table/privacy/test_dcr_utils.py
+++ tests/unit/single_table/privacy/test_dcr_utils.py
@@ -5,11 +5,8 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from sdmetrics._utils_metadata import _convert_datetime_columns
 from sdmetrics.single_table.privacy.dcr_utils import (
-    _calculate_dcr_between_row_and_data,
-    _calculate_dcr_between_rows,
-    _calculate_dcr_value,
+    _process_dcr_chunk,
     calculate_dcr,
 )
 from tests.utils import check_if_value_in_threshold
@@ -77,48 +74,6 @@ def column_ranges():
 
 
 @pytest.fixture()
-def expected_row_comparisons():
-    return [
-        0.6,
-        0.6,
-        0.666666,
-        0.555555,
-        0.966666,
-        0.188888,
-        0.616666,
-        0.616666,
-        0.633333,
-        0.227777,
-        0.916666,
-        0.727777,
-        0.399999,
-        0.866666,
-        0.5,
-        0.866667,
-        0.566667,
-        0.933333,
-        0.547777,
-        0.658888,
-        0.755555,
-        0.274444,
-        0.93,
-        0.574444,
-        0.665555,
-        0.221111,
-        0.722222,
-        0.71,
-        0.643333,
-        0.866666,
-        0.7222222,
-        0.944444,
-        0.544444,
-        0.833333,
-        0.833333,
-        0.4,
-    ]
-
-
-@pytest.fixture()
 def expected_same_dcr_result():
     return pd.Series([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 
@@ -160,117 +115,6 @@ SECONDS_IN_DAY = 86400
 ACCURACY_THRESHOLD = 0.000001
 
 
-@pytest.mark.parametrize(
-    'synthetic_value, real_value, col_range, sdtype, expected_dist',
-    [
-        (2.0, 2.0, 10.0, 'numerical', 0.0),
-        (1.0, 2.0, 10.0, 'numerical', 0.1),
-        (2.0, 1.0, 10.0, 'numerical', 0.1),
-        (100.0, 1.0, 10.0, 'numerical', 1.0),
-        (None, 1.0, 10.0, 'numerical', 1.0),
-        (1.0, np.nan, 10.0, 'numerical', 1.0),
-        (np.nan, None, 10.0, 'numerical', 0.0),
-        ('A', 'B', None, 'categorical', 1.0),
-        ('B', 'B', None, 'categorical', 0.0),
-        ('B', 'A', None, 'categorical', 1.0),
-        (None, 'B', None, 'categorical', 1.0),
-        ('A', None, None, 'categorical', 1.0),
-        (None, None, None, 'categorical', 0.0),
-        (0, None, None, 'categorical', 1.0),
-        (np.nan, None, None, 'categorical', 0.0),
-        (np.nan, np.nan, None, 'categorical', 0.0),
-        (True, False, None, 'boolean', 1.0),
-        (True, True, None, 'boolean', 0.0),
-        (False, True, None, 'boolean', 1.0),
-        (True, None, None, 'boolean', 1.0),
-        (datetime(2025, 1, 1).timestamp(), None, SECONDS_IN_DAY, 'datetime', 1.0),
-        (None, datetime(2025, 1, 1).timestamp(), SECONDS_IN_DAY, 'datetime', 1.0),
-        (
-            datetime(2025, 1, 1).timestamp(),
-            datetime(2025, 1, 1).timestamp(),
-            SECONDS_IN_DAY,
-            'datetime',
-            0.0,
-        ),
-        (
-            datetime(2025, 1, 2).timestamp(),
-            datetime(2025, 1, 1).timestamp(),
-            2 * SECONDS_IN_DAY,
-            'datetime',
-            0.5,
-        ),
-        (
-            datetime(2025, 10, 10).timestamp(),
-            datetime(2025, 1, 1).timestamp(),
-            SECONDS_IN_DAY,
-            'datetime',
-            1.0,
-        ),
-    ],
-)
-def test__calculate_dcr_value(synthetic_value, real_value, col_range, sdtype, expected_dist):
-    """Test _calculate_dcr_value with different types of values."""
-    # Run
-    dist = _calculate_dcr_value(synthetic_value, real_value, sdtype, col_range)
-
-    # Assert
-    assert dist == expected_dist
-
-
-def test__calculate_dcr_value_missing_range():
-    """Test _calculate_dcr_value with missing range for numerical values."""
-    # Setup
-    error_message = (
-        'No col_range was provided. The col_range is required '
-        'for numerical and datetime sdtype DCR calculation.'
-    )
-
-    # Assert
-    with pytest.raises(ValueError, match=error_message):
-        _calculate_dcr_value(1, 1, 'numerical', None)
-
-
-def test__calculate_dcr_between_rows(
-    real_data, synthetic_data, test_metadata, column_ranges, expected_row_comparisons
-):
-    """Test _calculate_dcr_between_rows for all row combinations"""
-    # Setup
-    result = []
-    real_data = _convert_datetime_columns(real_data, test_metadata)
-    synthetic_data = _convert_datetime_columns(synthetic_data, test_metadata)
-
-    # Run
-    for _, s_row_obj in synthetic_data.iterrows():
-        for _, t_row_obj in real_data.iterrows():
-            dist = _calculate_dcr_between_rows(s_row_obj, t_row_obj, column_ranges, test_metadata)
-            result.append(dist)
-
-    # Assert
-    for i in range(len(expected_row_comparisons)):
-        check_if_value_in_threshold(result[i], expected_row_comparisons[i], ACCURACY_THRESHOLD)
-
-
-def test__calculate_dcr_between_row_and_data(
-    real_data, synthetic_data, column_ranges, test_metadata, expected_dcr_result
-):
-    """Test _calculate_dcr_between_row_and_data for all rows."""
-    # Setup
-    result = []
-    real_data = _convert_datetime_columns(real_data, test_metadata)
-    synthetic_data = _convert_datetime_columns(synthetic_data, test_metadata)
-
-    # Run
-    for _, s_row_obj in synthetic_data.iterrows():
-        dist = _calculate_dcr_between_row_and_data(
-            s_row_obj, real_data, column_ranges, test_metadata
-        )
-        result.append(dist)
-
-    # Assert
-    for i in range(len(expected_dcr_result)):
-        check_if_value_in_threshold(result[i], expected_dcr_result[i], ACCURACY_THRESHOLD)
-
-
 def test_calculate_dcr(
     real_data,
     synthetic_data,
@@ -281,10 +125,10 @@ def test_calculate_dcr(
     """Calculate the DCR for all rows in a dataset against a traning dataset."""
     # Run
     result_dcr = calculate_dcr(
-        synthetic_data=synthetic_data, real_data=real_data, metadata=test_metadata
+        dataset=synthetic_data, reference_dataset=real_data, metadata=test_metadata
     )
     result_same_dcr = calculate_dcr(
-        synthetic_data=synthetic_data, real_data=synthetic_data, metadata=test_metadata
+        dataset=synthetic_data, reference_dataset=synthetic_data, metadata=test_metadata
     )
 
     # Assert
@@ -305,7 +149,7 @@ def test_calculate_dcr_different_cols_in_metadata(real_data, synthetic_data, tes
 
     # Run
     result = calculate_dcr(
-        synthetic_data=synthetic_data, real_data=real_data, metadata=test_metadata
+        dataset=synthetic_data, reference_dataset=real_data, metadata=test_metadata
     )
     expected_result = pd.Series([0.0, 0.1, 0.2, 0.02, 0.06, 0.0])
 
@@ -315,7 +159,7 @@ def test_calculate_dcr_different_cols_in_metadata(real_data, synthetic_data, tes
     test_metadata['columns'].pop('num_col')
     error_msg = 'There are no overlapping statistical columns to measure.'
     with pytest.raises(ValueError, match=error_msg):
-        calculate_dcr(synthetic_data=synthetic_data, real_data=real_data, metadata=test_metadata)
+        calculate_dcr(dataset=synthetic_data, reference_dataset=real_data, metadata=test_metadata)
 
 
 def test_calculate_dcr_with_shuffled_data():
@@ -333,9 +177,9 @@ def test_calculate_dcr_with_shuffled_data():
     metadata = {'columns': {'num_col': {'sdtype': 'numerical'}}}
 
     # Run
-    result = calculate_dcr(synthetic_data=synthetic_df, real_data=real_df, metadata=metadata)
+    result = calculate_dcr(dataset=synthetic_df, reference_dataset=real_df, metadata=metadata)
     result_shuffled = calculate_dcr(
-        synthetic_data=synthetic_df_shuffled, real_data=real_df_shuffled, metadata=metadata
+        dataset=synthetic_df_shuffled, reference_dataset=real_df_shuffled, metadata=metadata
     )
 
     # Assert
@@ -368,9 +212,38 @@ def test_calculate_dcr_with_zero_range():
     metadata = {'columns': {'num_col': {'sdtype': 'numerical'}, 'date_col': {'sdtype': 'datetime'}}}
 
     # Run and Assert
-    result = calculate_dcr(real_data=real_df, synthetic_data=synthetic_df_diff, metadata=metadata)
+    result = calculate_dcr(reference_dataset=real_df, dataset=synthetic_df_diff, metadata=metadata)
     assert result[0] == 1.0
-    result = calculate_dcr(real_data=real_df, synthetic_data=synthetic_df_same, metadata=metadata)
+    result = calculate_dcr(reference_dataset=real_df, dataset=synthetic_df_same, metadata=metadata)
     assert result[0] == 0.0
-    result = calculate_dcr(real_data=real_df, synthetic_data=synthetic_df_half, metadata=metadata)
+    result = calculate_dcr(reference_dataset=real_df, dataset=synthetic_df_half, metadata=metadata)
     assert result[0] == 0.5
+
+
+def test__process_dcr_chunk(real_data, synthetic_data, test_metadata, column_ranges):
+    """Test '_process_dcr_chunk' to see if subset is properly dropped."""
+    # Setup
+    real_data = real_data.drop(columns=['datetime_str_col'])
+    synthetic_data = synthetic_data.drop(columns=['datetime_str_col'])
+    del test_metadata['columns']['datetime_str_col']
+    cols_to_keep = real_data.columns
+
+    synthetic_data['index'] = range(len(synthetic_data))
+    real_data['index'] = range(len(real_data))
+    rows_to_keep = 2
+    chunk = synthetic_data.iloc[0:rows_to_keep].copy()
+
+    # Run
+    result = _process_dcr_chunk(
+        chunk=chunk,
+        reference_copy=real_data,
+        cols_to_keep=cols_to_keep,
+        metadata=test_metadata,
+        ranges=column_ranges,
+    )
+
+    # Assert
+    assert isinstance(result, pd.Series)
+    expected_result_first = 0.213333
+    check_if_value_in_threshold(result[0], expected_result_first, 0.000001)
+    assert len(result) == rows_to_keep
