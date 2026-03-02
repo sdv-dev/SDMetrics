73c61907ebba62c9ae257d04f7295533aad7eb43
Frances Hartwell <frances@datacebo.com>
2026-03-02 11:42:57 -0500

    Update CardinalityBoundaryAdherence to be compatible with composite keys

--- tests/unit/column_pairs/statistical/test_cardinality_boundary_adherence.py
+++ tests/unit/column_pairs/statistical/test_cardinality_boundary_adherence.py
@@ -22,6 +22,33 @@ class TestCardinalityBoundaryAdherence:
         # Assert
         assert result == {'score': 0.6}
 
+    def test_compute_breakdown_composite_keys(self):
+        """Test the ``compute_breakdown`` method with composite keys."""
+        # Setup
+        real_parent_keys = pd.DataFrame({
+            'id1': ['id0', 'id0', 'id0', 'id1', 'id1', 'id1'],
+            'id2': [None, 0, 1, None, 0, 1],
+        })
+        real_foreign_keys = pd.DataFrame({
+            'fk1': ['id0'] * 4 + ['id1'] * 4,
+            'fk2': [None, 0, 0, 1, None, 0, 1, 1],
+        })
+        real_data = (real_parent_keys, real_foreign_keys)
+        synthetic_parent_keys = real_parent_keys.copy()
+        synthetic_foreign_keys = pd.DataFrame({
+            'fk1': ['id0'] * 4 + ['id1'] * 4,
+            'fk2': [0, 0, 0, 1, None, 0, 0, None]
+        })
+        synthetic_data = (synthetic_parent_keys, synthetic_foreign_keys)
+
+        metric = CardinalityBoundaryAdherence()
+
+        # Run
+        result = metric.compute_breakdown(real_data, synthetic_data)
+
+        # Assert
+        assert result == {'score': 0.5}
+
     def test_compute(self):
         """Test the ``compute`` method."""
         # Setup
