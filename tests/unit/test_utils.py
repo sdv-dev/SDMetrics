91355cc50413eab0d81fdbaa3d2cf14ef604195a
Frances Hartwell <frances@datacebo.com>
2026-03-03 11:09:22 -0500

    Add support for composite keys for CardinalityShapeSimilarity metric

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -3,6 +3,7 @@ from unittest.mock import Mock, patch
 
 import numpy as np
 import pandas as pd
+import pytest
 
 from sdmetrics.utils import (
     HyperTransformer,
@@ -15,22 +16,23 @@ from sdmetrics.utils import (
 )
 
 
-def test_get_cardinality_distribution():
-    """Test the ``get_cardinality_distribution`` utility function.
-
-    Input:
-    - parent column
-    - child column
-
-    Output:
-    - the expected cardinality distribution.
-    """
-    # Setup
-    parent_column = pd.Series([1, 2, 3, 4, 5])
-    child_column = pd.Series([3, 4, 1, 4, 4, 5, 1])
-
+@pytest.mark.parametrize(
+    ('parent', 'child'),
+    [
+        [pd.Series([1, 2, 3, 4, 5]), pd.Series([3, 4, 1, 4, 4, 5, 1])],
+        [
+            pd.DataFrame({'id0': ['id0', 'id0', 'id1', 'id1', None], 'id1': [0, None, None, 1, 0]}),
+            pd.DataFrame({
+                'fk0': ['id0', 'id0', 'id1', 'id1', 'id1', 'id1', None],
+                'fk1': [0, 0, None, 1, 1, 1, 0],
+            }),
+        ],
+    ],
+)
+def test_get_cardinality_distribution(parent, child):
+    """Test the ``get_cardinality_distribution`` utility function."""
     # Run
-    cardinality_distribution = get_cardinality_distribution(parent_column, child_column)
+    cardinality_distribution = get_cardinality_distribution(parent, child)
 
     # Assert
     assert cardinality_distribution.to_list() == [2.0, 0.0, 1.0, 3.0, 1.0]
