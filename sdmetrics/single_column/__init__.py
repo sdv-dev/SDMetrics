657b510a658004fb0a8c33de0ecc55f43d166d8c
sarahmish <sarahalnegheimish@gmail.com>
2026-08-11 15:40:05 +0300

    add metric to list

--- sdmetrics/single_column/__init__.py
+++ sdmetrics/single_column/__init__.py
@@ -22,6 +22,7 @@ __all__ = [
     'CategoryCoverage',
     'CategoryAdherence',
     'CSTest',
+    'DatetimeFormatAdherence',
     'KeyUniqueness',
     'KSComplement',
     'MissingValueSimilarity',
