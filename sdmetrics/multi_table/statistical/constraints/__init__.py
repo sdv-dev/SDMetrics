58154d30784b5ff5ab99ef3af41fdf227539ab13
sarahmish <sarahalnegheimish@gmail.com>
2026-08-21 13:55:52 +0300

    add one hot encoding

--- sdmetrics/multi_table/statistical/constraints/__init__.py
+++ sdmetrics/multi_table/statistical/constraints/__init__.py
@@ -2,5 +2,6 @@
 
 from sdmetrics.multi_table.statistical.constraints.base import BaseConstraint
 from sdmetrics.multi_table.statistical.constraints.denormalized_table import DenormalizedTable
+from sdmetrics.multi_table.statistical.constraints.one_hot_encoding import OneHotEncoding
 
-__all__ = (BaseConstraint, DenormalizedTable)
+__all__ = (BaseConstraint, DenormalizedTable, OneHotEncoding)
