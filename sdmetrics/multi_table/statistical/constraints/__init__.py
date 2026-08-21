3b9b1faffa9357ac704caa40287b114bd575465e
sarahmish <sarahalnegheimish@gmail.com>
2026-08-21 17:51:40 +0300

    add inequality

--- sdmetrics/multi_table/statistical/constraints/__init__.py
+++ sdmetrics/multi_table/statistical/constraints/__init__.py
@@ -3,6 +3,7 @@
 from sdmetrics.multi_table.statistical.constraints.base import BaseConstraint
 from sdmetrics.multi_table.statistical.constraints.denormalized_table import DenormalizedTable
 from sdmetrics.multi_table.statistical.constraints.one_hot_encoding import OneHotEncoding
+from sdmetrics.multi_table.statistical.constraints.inequality import Inequality
 from sdmetrics.multi_table.statistical.constraints.range import Range
 
 __all__ = (BaseConstraint, DenormalizedTable, OneHotEncoding, Range)
