6be5fc59273ad3d6df387d050d6c4a2e982c4408
sarahmish <sarahalnegheimish@gmail.com>
2026-08-26 14:38:41 +0300

    remove multi-table constraints

--- sdmetrics/multi_table/statistical/constraints/__init__.py
+++ sdmetrics/multi_table/statistical/constraints/__init__.py
@@ -1,35 +1,31 @@
 """Constraints."""
 
 from sdmetrics.multi_table.statistical.constraints.base import BaseConstraint
-from sdmetrics.multi_table.statistical.constraints.carry_over_columns import CarryOverColumns
 from sdmetrics.multi_table.statistical.constraints.column_formula import ColumnFormula
-from sdmetrics.multi_table.statistical.constraints.denormalized_table import DenormalizedTable
-from sdmetrics.multi_table.statistical.constraints.one_hot_encoding import OneHotEncoding
-from sdmetrics.multi_table.statistical.constraints.inequality import Inequality
 from sdmetrics.multi_table.statistical.constraints.chained_inequality import ChainedInequality
-from sdmetrics.multi_table.statistical.constraints.range import Range
-from sdmetrics.multi_table.statistical.constraints.mixed_scales import MixedScales
-from sdmetrics.multi_table.statistical.constraints.fixed_increments import FixedIncrements
+from sdmetrics.multi_table.statistical.constraints.inequality import Inequality
 from sdmetrics.multi_table.statistical.constraints.fixed_combinations import FixedCombinations
+from sdmetrics.multi_table.statistical.constraints.fixed_increments import FixedIncrements
 from sdmetrics.multi_table.statistical.constraints.fixed_null_combinations import (
     FixedNullCombinations,
 )
+from sdmetrics.multi_table.statistical.constraints.mixed_scales import MixedScales
+from sdmetrics.multi_table.statistical.constraints.one_hot_encoding import OneHotEncoding
+from sdmetrics.multi_table.statistical.constraints.range import Range
 from sdmetrics.multi_table.statistical.constraints.referential_hierarchy import (
     SelfReferentialHierarchy,
 )
 
 __all__ = (
     BaseConstraint,
-    DenormalizedTable,
-    OneHotEncoding,
-    Range,
-    Inequality,
+    ColumnFormula,
     ChainedInequality,
+    Inequality,
     FixedIncrements,
     FixedCombinations,
     FixedNullCombinations,
-    ColumnFormula,
-    CarryOverColumns,
     MixedScales,
+    OneHotEncoding,
+    Range,
     SelfReferentialHierarchy,
 )
